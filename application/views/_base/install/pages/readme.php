49cdd983a219aed700b3ad4339093df7de511274
David VanScott <davidv@anodyne-productions.com>
2009-10-26 15:12:46 -0400

    updated the readme for the install section and the update section

--- application/views/_base/install/pages/readme.php
+++ application/views/_base/install/pages/readme.php
@@ -1,16 +1,18 @@
-<p><strong>Please read this document in its entirety before attempting to install Nova. If you do not understand this document in part or in its entirety, please ask your question in the <a href="http://forums.anodyne-productions.com/" target="_blank">Anodyne Support forums</a> before attempting to install.</strong></p>
+<h2>Server Requirements</h2>
 
-<h3>Requirements</h3>
+<ul class="square">
+	<li>PHP 4.3.2 or higher</li>
+	<li>MySQL 4.1 or higher</li>
+	<li>Apache (recommended, but any PHP-supported web server will work)</li>
+	<li>Register globals should be turned off</li>
+	<li>Memory limit of 8M or higher</li>
+</ul>
 
-<p>PHP 4.3.2+<br />
-MySQL 4.1+<br />
-Apache recommended, but should run on any PHP-supported web server<br />
-Server running OS X, Linux, UNIX, or Windows (while using a Windows server is supported, we do not recommended it)</p>
+<p>If you are new to PHP or website management, you cannot run Nova on your local machine without a web server installed.  You must have either a hosting provider with the above requirements or a local server (XAMPP, WAMP, MAMP, LAMP, etc.) to install and use Nova. If you are unsure whether your server will allow you to run Nova, you can run the <?php echo anchor('install/verify', 'verification tool');?> before beginning.</p>
 
-<p>If you are new to PHP or website management, you cannot run this system (the files and folders contained within the zip file you just downloaded) on your local machine without a web server installed.  You must have either a hosting provider with the above requirements or a local server (XAMPP, WAMP, MAMP, LAMP, etc.) to install and use Nova. If you are unsure whether your server will allow you to run Nova, you can run the <?php echo anchor('install/verify', 'verification tool');?> before beginning.</p>
+<h2>Clean Install Guide</h2>
 
-<h3>Clean Install Guide</h3>
-<p>In order to proceed, you will need the following items:</p>
+<p>If you want to do a fresh install of Nova, you should read the <a href="http://docs.anodyne-productions.com/index.php/nova/overview/install" target="_blank">install guide</a> before you begin. If you don't understand something in the install guide, you should ask any questions you have on the <a href="http://forums.anodyne-productions.com" target="_blank">Anodyne support forums</a> before you attempt to install Nova. If you've read the install guide and are ready to proceed, you'll need the following items:</p>
 
 <ul class="square">
 	<li>A webserver with at least 10MB of disk space</li>
@@ -19,9 +21,17 @@ Server running OS X, Linux, UNIX, or Windows (while using a Windows server is su
 	<li>Basic information about the primary character you want to play</li>
 </ul>
 
-<p>If you have all this information, you can continue to the <?php echo anchor('install/step/1', 'installation');?> now.</p>
+<p class="fontMedium bold"><?php echo anchor('install/step/1', 'Go to Step 1 &raquo;');?></p>
 
-<h3>Upgrade Guide</h3>
-<p>Nova does not currently support upgrading from SMS. In future milestones, we&rsquo;ll have a more complete upgrade package. Your site will need to be running the latest version of SMS 2.6 in order to be upgraded. If you do not have the latest version of SMS 2.6 on your site now, we encourage you to start by upgrading it. You will then be able to test the upgrade process with data from your sim&rsquo;s site. More information about upgrades will be available in future milestones.</p>
+<h2>Upgrade Guide</h2>
 
-<p><?php echo anchor('install/index', $label['back'], array('class' => 'fontMedium bold'));?></p>
+<p>If you want to upgrade from SMS 2 to Nova, you should read the <a href="http://docs.anodyne-productions.com/index.php/nova/overview/upgrade" target="_blank">upgrade guide</a> before you begin. If you don't understand something in the upgrade guide, you should ask any questions you have on the <a href="http://forums.anodyne-productions.com" target="_blank">Anodyne support forums</a> before you attempt to upgrade SMS to Nova. If you've read the upgrade guide and are ready to proceed, you'll need the following items:</p>
+
+<ul class="square">
+	<li>A webserver with at least 10MB of disk space</li>
+	<li>A MySQL database</li>
+	<li>An installation of SMS <?php echo SMS_UPGRADE_VERSION;?> in the same database you're installing Nova</li>
+	<li>Your database connection information you received from your host</li>
+</ul>
+
+<p class="fontMedium bold"><?php echo anchor('upgrade/index', 'Go to the Upgrade Center &raquo;');?></p>
