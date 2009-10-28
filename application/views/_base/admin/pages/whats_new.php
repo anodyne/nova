35d5d407f3540f0503d47fed2dd4a9dc8e40e342
David VanScott <davidv@anodyne-productions.com>
2009-10-28 16:09:25 -0400

    updated nova to have a whats new page (modal window version and normal page version)

--- application/views/_base/admin/pages/whats_new.php
+++ application/views/_base/admin/pages/whats_new.php
@@ -1,14 +1,3 @@
-<style type="text/css">
-	body {
-		margin: 0;
-		padding: .5em;
-		line-height: 1.4;
-		
-		background-color: #111;
-		color: #eee;
-		font-family: "Lucida Grande", "Verdana", Arial, sans-serif;
-		font-size: 11px;
-	}
-</style>
+<?php echo text_output($header, 'h1', 'page-head');?>
 
-<?=$whats_new;?>
+<?php echo text_output($whats_new);?>
