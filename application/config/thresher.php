013ec7abc7f33c3aa7cbb09b6f5c2ffd1800330c
David VanScott <davidv@anodyne-productions.com>
2009-11-02 16:26:11 -0500

    added the thresher library and config file

---
+++ application/config/thresher.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+| -------------------------------------------------------------------
+|  THRESHER SETTINGS
+| -------------------------------------------------------------------
+| You can change the way that Thresher stores and parses content data
+| by changing the variable below. Your options are html, bbcode and wiki.
+| At a later time, we may add additional types.
+|
+| NOTE(1): Once you have set this one, you should not change it. Doing so
+| will mess up how your wiki pages are parsed
+*/
+
+$config['parsetype'] = 'html';
+
+/* End of file thresher.php */
+/* Location: ./application/config/thresher.php */
