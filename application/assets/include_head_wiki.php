0168553d85500d643515a0b7707ea6b4f9bca243
David VanScott <davidv@anodyne-productions.com>
2009-11-02 16:27:20 -0500

    updated the wiki head include document

--- application/assets/include_head_wiki.php
+++ application/assets/include_head_wiki.php
@@ -11,4 +11,44 @@
 | system to use.
 |
 */
-?><script type="text/javascript" src="<?php echo base_url() . APPFOLDER .'/assets/js/jquery.js';?>"></script>
+?><style type="text/css">
+			<?php if (!is_file(APPPATH .'views/'. $current_skin .'/admin/css/jquery.facebox.css')): ?>
+				@import url("<?php echo base_url() . APPFOLDER .'/assets/js/css/jquery.facebox.css';?>");
+			<?php else: ?>
+				@import url("<?php echo base_url() . APPFOLDER .'/views/'. $current_skin .'/admin/css/jquery.facebox.css';?>");
+			<?php endif;?>
+			
+			@import url("<?php echo base_url() . APPFOLDER .'/assets/js/css/ui.core.css';?>");
+			
+			<?php if (!is_file(APPPATH .'views/'. $current_skin .'/admin/css/ui.theme.css')): ?>
+				@import url("<?php echo base_url() . APPFOLDER .'/assets/js/css/ui.theme.css';?>");
+			<?php else: ?>
+				@import url("<?php echo base_url() . APPFOLDER .'/views/'. $current_skin .'/admin/css/ui.theme.css';?>");
+			<?php endif;?>
+		</style>
+			
+		<script type="text/javascript" src="<?php echo base_url() . APPFOLDER .'/assets/js/jquery.js';?>"></script>
+		<script type="text/javascript" src="<?php echo base_url() . APPFOLDER .'/assets/js/jquery.lazy.js';?>"></script>
+		<script type="text/javascript" src="<?php echo base_url() . APPFOLDER .'/assets/js/ui.core.min.js';?>"></script>
+		<script type="text/javascript" src="<?php echo base_url() . APPFOLDER .'/assets/js/ui.tabs.min.js';?>"></script>
+		<script type="text/javascript" src="<?php echo base_url() . APPFOLDER .'/assets/js/jquery.facebox.js';?>"></script>
+		
+		<script type="text/javascript">
+			$(document).ready(function(){
+				$('a#userpanel').toggle(function(){
+					$('div.panel-body').slideDown('normal', function(){
+						$('.panel-trigger div.ui-icon').removeClass('ui-icon-triangle-1-s');
+						$('.panel-trigger div.ui-icon').addClass('ui-icon-triangle-1-n');
+					});
+					return false;
+				}, function(){
+					$('div.panel-body').slideUp('normal', function(){
+						$('.panel-trigger div.ui-icon').removeClass('ui-icon-triangle-1-n');
+						$('.panel-trigger div.ui-icon').addClass('ui-icon-triangle-1-s');
+					});
+					return false;
+				});
+				
+				$.facebox.settings.loadingImage = '<?php echo base_url() . APPFOLDER;?>/assets/js/images/facebox-loading.gif';
+			});
+		</script>
