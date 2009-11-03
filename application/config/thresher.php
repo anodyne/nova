f1a2a7e8f0532b9ed625ce52dcf45d60fc04c414
David VanScott <davidv@anodyne-productions.com>
2009-11-03 15:45:04 -0500

    worked on the thresher library and the necessary files

--- application/config/thresher.php
+++ application/config/thresher.php
@@ -4,8 +4,14 @@
 |  THRESHER SETTINGS
 | -------------------------------------------------------------------
 | You can change the way that Thresher stores and parses content data
-| by changing the variable below. Your options are html, bbcode and wiki.
-| At a later time, we may add additional types.
+| by changing the variable below. Your options are:
+|
+| html			Any HTML can be used in the body of wiki pages
+| bbcode		Any BBCode can be used in the body of wiki pages
+| markdown		More information about Markdown can be found at its
+|				site: http://daringfireball.net/projects/markdown/
+| textile		More information about Textile can be found at their
+|				site: http://textile.thresholdstate.com/
 |
 | NOTE(1): Once you have set this one, you should not change it. Doing so
 | will mess up how your wiki pages are parsed
