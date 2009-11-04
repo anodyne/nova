285a863cb5b5e4699be24e5ce577d401e4f2fae1
David VanScott <davidv@anodyne-productions.com>
2009-11-04 16:11:39 -0500

    updated the wiki head include file with the necessary components

--- application/assets/include_head_wiki.php
+++ application/assets/include_head_wiki.php
@@ -11,6 +11,13 @@
 | system to use.
 |
 */
+
+/* pull in the config file */
+$this->load->config('thresher');
+
+/* grab the type of parsing */
+$parse = $this->config->item('parsetype');
+
 ?><style type="text/css">
 			<?php if (!is_file(APPPATH .'views/'. $current_skin .'/admin/css/jquery.facebox.css')): ?>
 				@import url("<?php echo base_url() . APPFOLDER .'/assets/js/css/jquery.facebox.css';?>");
@@ -25,6 +32,9 @@
 			<?php else: ?>
 				@import url("<?php echo base_url() . APPFOLDER .'/views/'. $current_skin .'/admin/css/ui.theme.css';?>");
 			<?php endif;?>
+			
+			@import url("<?php echo base_url() . APPFOLDER .'/assets/js/markitup/skins/simple/style.css';?>");
+			@import url("<?php echo base_url() . APPFOLDER .'/assets/js/markitup/sets/'. $parse .'/style.css';?>");
 		</style>
 			
 		<script type="text/javascript" src="<?php echo base_url() . APPFOLDER .'/assets/js/jquery.js';?>"></script>
@@ -32,6 +42,10 @@
 		<script type="text/javascript" src="<?php echo base_url() . APPFOLDER .'/assets/js/ui.core.min.js';?>"></script>
 		<script type="text/javascript" src="<?php echo base_url() . APPFOLDER .'/assets/js/ui.tabs.min.js';?>"></script>
 		<script type="text/javascript" src="<?php echo base_url() . APPFOLDER .'/assets/js/jquery.facebox.js';?>"></script>
+		
+		<script type="text/javascript" src="<?php echo base_url() . APPFOLDER .'/assets/js/markitup/jquery.markitup.js';?>"></script>
+		<script type="text/javascript" src="<?php echo base_url() . APPFOLDER .'/assets/js/markitup/sets/'. $parse .'/set.js';?>"></script>
+
 		
 		<script type="text/javascript">
 			$(document).ready(function(){
@@ -50,5 +64,7 @@
 				});
 				
 				$.facebox.settings.loadingImage = '<?php echo base_url() . APPFOLDER;?>/assets/js/images/facebox-loading.gif';
+				
+				$('.markitup').markItUp(mySettings);
 			});
 		</script>
