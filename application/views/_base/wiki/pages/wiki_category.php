9899aa2503fa1240b596fd3bdfa9c5197abec8d4
David VanScott <davidv@anodyne-productions.com>
2009-11-04 16:10:37 -0500

    updated the wiki/category page to use the proper link to pages

--- application/views/_base/wiki/pages/wiki_category.php
+++ application/views/_base/wiki/pages/wiki_category.php
@@ -5,7 +5,7 @@
 <?php if (isset($pages)): ?>
 	<ul class="square margin1 padding1">
 	<?php foreach ($pages as $p): ?>
-		<li><?php echo anchor('wiki/page/'. $p['id'], $p['title']);?></li>
+		<li><?php echo anchor('wiki/view/page/'. $p['id'], $p['title']);?></li>
 	<?php endforeach;?>
 <?php else: ?>
 	<?php echo text_output($label['nopages'], 'h3', 'orange');?>
