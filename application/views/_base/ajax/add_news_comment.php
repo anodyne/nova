35b50fb935fb2b2573a6e98d5f7ebb953752a784
David VanScott <davidv@anodyne-productions.com>
2009-11-17 14:45:53 -0500

    removed the old ajax views from the ajax folder

--- application/views/_base/ajax/add_news_comment.php
+++
@@ -1,7 +0,0 @@
-<?php echo text_output($header, 'h2');?>
-<p>
-	<?php echo form_open('main/viewnews/'. $news_id);?>
-		<?php echo form_textarea($inputs['comment_text']);?><br /><br />
-		<?php echo form_button($inputs['comment_button']);?>
-	<?php echo form_close();?><br />
-</p>
--- application/views/_base/ajax/whats_new.php
+++
@@ -1,3 +0,0 @@
-<?php echo text_output($header, 'h2');?>
-
-<?php echo text_output($whats_new);?>
