6f14fae5245910687fb78ab42d1f105e221f35a8
David VanScott <davidv@anodyne-productions.com>
2009-11-24 16:13:20 -0500

    updated the MY_date_helper to throw the word "ago" on to the end of the string

--- application/helpers/MY_date_helper.php
+++ application/helpers/MY_date_helper.php
@@ -48,90 +48,97 @@ if ( ! function_exists('timespan_short'))
 		$CI =& get_instance();
 		$CI->lang->load('date');
 		$CI->lang->load('app');
-
-		if ( ! is_numeric($seconds))
-		{
-			$seconds = 1;
-		}
-	
-		if ( ! is_numeric($time))
-		{
-			$time = time();
-		}
-	
-		if ($time <= $seconds)
-		{
-			$seconds = 1;
-		}
-		else
-		{
-			$seconds = $time - $seconds;
-		}
 		
-		$str = '';
-		$years = floor($seconds / 31536000);
-	
-		if ($years > 0)
-		{	
-			$str .= $years.' '.$CI->lang->line((($years	> 1) ? 'date_years' : 'date_year')).', ';
-		}	
-	
-		$seconds -= $years * 31536000;
-		$months = floor($seconds / 2628000);
-	
-		if ($years > 0 OR $months > 0)
+		if ($seconds !== NULL)
 		{
-			if ($months > 0)
-			{	
-				$str .= $months.' '.$CI->lang->line((($months	> 1) ? 'date_months' : 'date_month')).', ';
-			}	
-	
-			$seconds -= $months * 2628000;
-		}
-
-		$weeks = floor($seconds / 604800);
-	
-		if ($years > 0 OR $months > 0 OR $weeks > 0)
-		{
-			if ($weeks > 0)
-			{	
-				$str .= $weeks.' '.$CI->lang->line((($weeks	> 1) ? 'date_weeks' : 'date_week')).', ';
-			}
-		
-			$seconds -= $weeks * 604800;
-		}			
-
-		$days = floor($seconds / 86400);
-	
-		if ($months > 0 OR $weeks > 0 OR $days > 0)
-		{
-			if ($days > 0)
-			{	
-				$str .= $days.' '.$CI->lang->line((($days	> 1) ? 'date_days' : 'date_day')).', ';
-			}
-	
-			$seconds -= $days * 86400;
-		}
-	
-		$hours = floor($seconds / 3600);
-	
-		if ($days > 0 OR $hours > 0)
-		{
-			if ($hours > 0)
+			if ( ! is_numeric($seconds))
 			{
-				$str .= $hours.' '.$CI->lang->line((($hours	> 1) ? 'date_hours' : 'date_hour')).', ';
+				$seconds = 1;
 			}
 		
-			$seconds -= $hours * 3600;
+			if ( ! is_numeric($time))
+			{
+				$time = time();
+			}
+		
+			if ($time <= $seconds)
+			{
+				$seconds = 1;
+			}
+			else
+			{
+				$seconds = $time - $seconds;
+			}
+			
+			$str = '';
+			$years = floor($seconds / 31536000);
+		
+			if ($years > 0)
+			{	
+				$str .= $years.' '.$CI->lang->line((($years	> 1) ? 'date_years' : 'date_year')).', ';
+			}	
+		
+			$seconds -= $years * 31536000;
+			$months = floor($seconds / 2628000);
+		
+			if ($years > 0 OR $months > 0)
+			{
+				if ($months > 0)
+				{	
+					$str .= $months.' '.$CI->lang->line((($months	> 1) ? 'date_months' : 'date_month')).', ';
+				}	
+		
+				$seconds -= $months * 2628000;
+			}
+	
+			$weeks = floor($seconds / 604800);
+		
+			if ($years > 0 OR $months > 0 OR $weeks > 0)
+			{
+				if ($weeks > 0)
+				{	
+					$str .= $weeks.' '.$CI->lang->line((($weeks	> 1) ? 'date_weeks' : 'date_week')).', ';
+				}
+			
+				$seconds -= $weeks * 604800;
+			}			
+	
+			$days = floor($seconds / 86400);
+		
+			if ($months > 0 OR $weeks > 0 OR $days > 0)
+			{
+				if ($days > 0)
+				{	
+					$str .= $days.' '.$CI->lang->line((($days	> 1) ? 'date_days' : 'date_day')).', ';
+				}
+		
+				$seconds -= $days * 86400;
+			}
+		
+			$hours = floor($seconds / 3600);
+		
+			if ($days > 0 OR $hours > 0)
+			{
+				if ($hours > 0)
+				{
+					$str .= $hours.' '.$CI->lang->line((($hours	> 1) ? 'date_hours' : 'date_hour')).', ';
+				}
+			
+				$seconds -= $hours * 3600;
+			}
+			else
+			{
+				return ucwords($CI->lang->line('labels_less') .' '. $CI->lang->line('labels_than')) . ' 1 '. $CI->lang->line('date_hour');
+			}
+		
+			$minutes = floor($seconds / 60);
+				
+			return substr(trim($str), 0, -1) .' '. $CI->lang->line('time_ago');
 		}
 		else
 		{
-			return ucwords($CI->lang->line('labels_less') .' '. $CI->lang->line('labels_than')) . ' 1 '. $CI->lang->line('date_hour');
+			return ucwords($CI->lang->line('labels_no') .' '. $CI->lang->line('labels_data') .' '. $CI->lang->line('labels_available'));
 		}
-	
-		$minutes = floor($seconds / 60);
-			
-		return substr(trim($str), 0, -1);
 	}
 }
 
