67d0280ffdc5912d7b423520496619c878d53d32
David VanScott <davidv@anodyne-productions.com>
2009-11-30 11:23:11 -0500

    updated the rss feed controller to use language items and not hardcode references

--- application/controllers/base/feed_base.php
+++ application/controllers/base/feed_base.php
@@ -41,6 +41,9 @@ class Feed_base extends Controller {
 		/* set the template */
 		$this->template->set_template('rss');
 		$this->template->set_master_template('_base/template_rss.php');
+		
+		/* set and load the language file needed */
+		$this->lang->load('app');
 	}
 	
 	function logs()
@@ -63,7 +66,8 @@ class Feed_base extends Controller {
 				$data['entries'][$i]['title'] = $log->log_title;
 				$data['entries'][$i]['date'] = $log->log_date;
 				
-				$log_header = 'A personal log by '. $this->char->get_character_name($log->log_author_character, TRUE) ."\r\n\r\n";
+				$log_header = ucfirst(lang('labels_a') .' '. lang('personallog') .' '. lang('labels_by'));
+				$log_header.= ' '. $this->char->get_character_name($log->log_author_character, TRUE) ."\r\n\r\n";
 				
 				$data['entries'][$i]['content'] = nl2br($log_header . $log->log_content);
 				
@@ -102,8 +106,9 @@ class Feed_base extends Controller {
 				$data['entries'][$i]['title'] = $item->news_title;
 				$data['entries'][$i]['date'] = $item->news_date;
 				
-				$news_header = 'A news item by '. $this->char->get_character_name($item->news_author_character, TRUE) ."\r\n";
-				$news_header.= "<b>Category</b> - ". $item->newscat_name ."\r\n\r\n";
+				$news_header = ucfirst(lang('labels_a') .' '. lang('newsitem') .' '. lang('labels_by'));
+				$news_header.= ' '. $this->char->get_character_name($item->news_author_character, TRUE) ."\r\n";
+				$news_header.= "<b>". ucfirst(lang('labels_category')) ."</b> - ". $item->newscat_name ."\r\n\r\n";
 				
 				$data['entries'][$i]['content'] = nl2br($news_header . $item->news_content);
 				
@@ -152,10 +157,11 @@ class Feed_base extends Controller {
 				}
 				
 				/* break the array into a string */
-				$post_header = 'A post by '. implode(', ', $authors['full_names']) ."\r\n\r\n";
-				$post_header.= "<b>Mission</b> - ". $this->mis->get_mission($post->post_mission, 'mission_title') ."\r\n";
-				$post_header.= (!empty($post->post_location)) ? "<b>Location</b> - ". $post->post_location ."\r\n" : '';
-				$post_header.= (!empty($post->post_timeline)) ? "<b>Timeline</b> - ". $post->post_timeline ."\r\n" : '';
+				$post_header = ucfirst(lang('labels_a') .' '. lang('missionpost') .' '. lang('labels_by'));
+				$post_header.= ' '. implode(', ', $authors['full_names']) ."\r\n\r\n";
+				$post_header.= "<b>". ucfirst(lang('global_mission')) ."</b> - ". $this->mis->get_mission($post->post_mission, 'mission_title') ."\r\n";
+				$post_header.= (!empty($post->post_location)) ? "<b>". ucfirst(lang('labels_location')) ."</b> - ". $post->post_location ."\r\n" : '';
+				$post_header.= (!empty($post->post_timeline)) ? "<b>". ucfirst(lang('labels_timeline')) ."</b> - ". $post->post_timeline ."\r\n" : '';
 				$post_header.= "\r\n";
 				
 				$data['entries'][$i]['content'] = nl2br($post_header . $post->post_content);
