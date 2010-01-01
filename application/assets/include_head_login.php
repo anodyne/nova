d28ceace01cef1cfc287afcc9fc228e0f8e53239
anodyne <davidv@anodyne-productions.com>
2010-01-01 12:06:54 -0500

    updated the login head include to pull in the countdown script

--- application/assets/include_head_login.php
+++ application/assets/include_head_login.php
@@ -12,3 +12,14 @@
 |
 */
 ?><script type="text/javascript" src="<?php echo base_url() . APPFOLDER .'/assets/js/jquery.js';?>"></script>
+		<script type="text/javascript" src="<?php echo base_url() . APPFOLDER .'/assets/js/jquery.countdown.js';?>"></script>
+		
+		<script type="text/javascript">
+			$(document).ready(function(){
+				$('#countdown').countDown({
+					startNumber: 5,
+					startFontSize: '1em',
+					endFontSize: '1em',
+				});
+			});
+		</script>
