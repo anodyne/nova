7a14626d3f0acf6a6829794ec072fcc964a1e683
anodyne <davidv@anodyne-productions.com>
2010-01-20 21:45:58 -0500

    updated the contact page to be laid out better

--- application/views/_base/main/pages/main_contact.php
+++ application/views/_base/main/pages/main_contact.php
@@ -1,42 +1,34 @@
 <?php echo text_output($header, 'h1', 'page-head');?>
 
-<?php echo text_output($msg);?>
+<?php echo text_output($msg);?><br />
 
 <?php echo form_open('main/contact');?>
-	<table class="table100">
-		<tr>
-			<td class="cell-label"><?php echo $label['send'];?></td>
-			<td class="cell-spacer"></td>
-			<td class="cell_content"><?php echo form_dropdown('to', $values['to'], 0);?></td>
-		</tr>
-		<?php echo table_row_spacer(3, 5);?>
-		<tr>
-			<td class="cell-label"><?php echo $label['name'];?></td>
-			<td class="cell-spacer"></td>
-			<td class="cell_content"><?php echo form_input($inputs['name']);?></td>
-		</tr>
-		<?php echo table_row_spacer(3, 5);?>
-		<tr>
-			<td class="cell-label"><?php echo $label['email'];?></td>
-			<td class="cell-spacer"></td>
-			<td class="cell_content"><?php echo form_input($inputs['email']);?></td>
-		</tr>
-		<?php echo table_row_spacer(3, 5);?>
-		<tr>
-			<td class="cell-label"><?php echo $label['subject'];?></td>
-			<td class="cell-spacer"></td>
-			<td class="cell_content"><?php echo form_input($inputs['subject']);?></td>
-		</tr>
-		<?php echo table_row_spacer(3, 5);?>
-		<tr>
-			<td class="cell-label"><?php echo $label['message'];?></td>
-			<td class="cell-spacer"></td>
-			<td class="cell_content"><?php echo form_textarea($inputs['message']);?></td>
-		</tr>
-		<?php echo table_row_spacer(3, 15);?>
-		<tr>
-			<td colspan="2"></td>
-			<td class="cell_content"><?php echo form_button($button['submit']);?></td>
-		</tr>
-	</table>
+	<p>
+		<strong><?php echo $label['send'];?></strong><br />
+		<?php echo form_dropdown('to', $values['to'], 0);?>
+	</p>
+	
+	<p>
+		<strong><?php echo $label['name'];?></strong><br />
+		<?php echo form_input($inputs['name']);?>
+	</p>
+	
+	<p>
+		<strong><?php echo $label['email'];?></strong><br />
+		<?php echo form_input($inputs['email']);?>
+	</p>
+	
+	<p>
+		<strong><?php echo $label['subject'];?></strong><br />
+		<?php echo form_input($inputs['subject']);?>
+	</p>
+	
+	<p>
+		<strong><?php echo $label['message'];?></strong><br />
+		<?php echo form_textarea($inputs['message']);?>
+	</p><br />
+	
+	<p>
+		<?php echo form_button($button['submit']);?>
+	</p>
 <?php echo form_close();?>
