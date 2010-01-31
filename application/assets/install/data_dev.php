a5de6a586826a24b558d0d50b2971eb59b341c17
anodyne <davidv@anodyne-productions.com>
2010-01-31 11:41:00 -0500

    updated the basic install data

--- application/assets/install/data_dev.php
+++ application/assets/install/data_dev.php
@@ -99,23 +99,23 @@ $access_groups = array(
 $access_roles = array(
 	array(
 		'role_name' => 'System Administrator',
-		'role_access' => '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,29,31,33,34,35,36,37,38,39,40,41,42,43,46,47,50,52,53,55,57,60,61',
+		'role_access' => '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,29,31,33,34,35,36,37,38,39,40,41,42,43,44,47,48,51,53,54,56,58,61,62',
 		'role_desc' => 'System administrators can take any action in the system. Only give this access level out to people you implicitly trust.'),
 	array(
 		'role_name' => 'Basic Administrator',
-		'role_access' => '1,2,3,4,5,6,7,8,21,29,31,33,35,37,38,39,40,41,42,43,46,47,50,52,53,55,57,60,61',
+		'role_access' => '1,2,3,4,5,6,7,8,21,29,31,33,35,37,38,39,40,41,42,43,44,47,48,51,53,54,56,58,61,62',
 		'role_desc' => 'Basic administrators have power to do many of the tasks system administrators do, but with some restrictions. This role is intended to be used by an assistant game manager or other highly senior players on the RPG.'),
 	array(
 		'role_name' => 'Power User',
-		'role_access' => '1,2,4,5,6,7,8,28,30,32,37,38,40,41,45,49,51,54,56,59',
+		'role_access' => '1,2,4,5,6,7,8,28,30,32,37,38,40,41,43,46,50,52,55,57,60',
 		'role_desc' => 'Power users are users that can take more action than a standard user. This role is intended to be used for senior players on the RPG.'),
 	array(
 		'role_name' => 'Standard User',
-		'role_access' => '1,2,4,5,6,7,8,28,30,32,37,38,40,48,51,54,56,58',
+		'role_access' => '1,2,4,5,6,7,8,28,30,32,37,38,40,43,49,52,55,57,59',
 		'role_desc' => 'Standard users are generally the majority of players. This role gives them access to all the pieces they will need to play the game and use the system.'),
 	array(
 		'role_name' => 'Inactive User',
-		'role_access' => '1,4,37,38,40,48,51,54',
+		'role_access' => '1,4,37,38,40,43,49,52,55',
 		'role_desc' => 'Inactive players are players that have left the RPG. Instead of being completely locked out, the player can log in and take some very limited actions.')
 );
 
@@ -340,6 +340,11 @@ $access_pages = array(
 		'page_url' => 'report/applications',
 		'page_group' => 5,
 		'page_desc' => "Can view a report on all applications submitted through the system"),
+	array(
+		'page_name' => "Sim Statistics",
+		'page_url' => 'report/stats',
+		'page_group' => 5,
+		'page_desc' => "Can view a report on sim statistics for the current and previous months"),
 		
 	array(
 		'page_name' => "Character Management",
@@ -1776,6 +1781,16 @@ $menu_items = array(
 		'menu_cat' => 'report',
 		'menu_use_access' => 'y',
 		'menu_access' => 'report/posting'),
+	array(
+		'menu_name' => 'Sim Statistics',
+		'menu_group' => 0,
+		'menu_order' => 2,
+		'menu_link' => 'report/stats',
+		'menu_sim_type' => 1,
+		'menu_type' => 'adminsub',
+		'menu_cat' => 'report',
+		'menu_use_access' => 'y',
+		'menu_access' => 'report/stats'),
 	array(
 		'menu_name' => 'Moderation',
 		'menu_group' => 1,
@@ -3455,7 +3470,7 @@ $system_info = array(
 		'sys_install_date' => now(),
 		'sys_version_major' => 0,
 		'sys_version_minor' => 9,
-		'sys_version_update' => 4)
+		'sys_version_update' => 5)
 );
 
 $system_versions = array(
@@ -3470,7 +3485,124 @@ $system_versions = array(
 		'version_major' => '0',
 		'version_minor' => '9',
 		'version_update' => '4',
-		'version_date' => now()),
+		'version_date' => 1264903200,
+		'version_launch'	=> 'Nova 0.9.4 is an update to the beta release of the next generation RPG management software from Anodyne Productions.',
+		'version_changes'	=> "* added the jquery.ui.mouse file
+* added the jquery.ui.widget file
+* added the 0.9.4 update file
+* updated the mission management page to use the datepicker
+* updated the version info in the constants file
+* updated the basic install data
+    * version information
+    * system information
+    * jquery component information
+    * jquery ui component information
+    * removed textboxlist from components list
+* updated the database schema
+    * added the wiki category description field
+* updated the index files in the core directory to use the proper line endings (unix) and encoding (utf8)
+* updated the beta skin
+    * added the skin.yml file
+    * updated the main logo files
+    * [admin] removed unused images
+    * [admin] updated the skin images
+    * [admin] updated the footer of the template
+    * [admin] updated the stylesheets
+        * added styling for accordion lists
+        * updated the skin.css file to match with some of the changes from the login's skin.css
+    * [login] removed unused image files
+    * [login] updated the image files
+    * [login] updated the stylesheets
+        * updated the styles to be cleaner and use better practices
+        * updated the skin.css file to remove unused styles
+        * updated the structure.css file to remove unused styles
+    * [main] removed unused images
+    * [main] updated the skin images
+    * [main] updated the footer of the template
+    * [main] updated the stylesheets
+        * updated the skin.css file to match with some of the changes from the login's skin.css
+    * [wiki] removed the textboxlist images
+    * [wiki] removed the textboxlist stylesheets
+    * [wiki] removed unused images
+    * [wiki] updated the skin images
+    * [wiki] updated the footer of the template
+    * [wiki] updated the stylesheets
+        * updated the skin.css file to match with some of the changes from the login's skin.css
+* updated the sunny skin
+    * updated the main logo files
+    * [admin] updated the stylesheets
+        * added styling for accordion lists
+    * [main] updated the stylesheets
+        * updated the alt row color
+        * added the info-full class
+    * [login] updated the stylesheets
+        * updated the skin.css file to match changes made to main's skin.css
+        * updated the skin.css file to remove unused styles
+        * updated the structure.css file to remove unused styles
+    * [login] removed the ui.theme.css file
+    * [login] removed the unused jquery ui theme images
+    * [wiki] added the proper images
+    * [wiki] removed unused images
+    * [wiki] removed the textboxlist images
+    * [wiki] removed the textboxlist stylesheets
+    * [wiki] updated the stylesheets
+        * updated to look like the main section
+        * updated the alt row color
+        * updated the textboxlist styles to remove the focus shadow
+        * added the markitup link fix
+* updated the lightness skin
+    * [admin] updated the stylesheets
+        * added styling for accordion lists
+    * [login] updated the stylesheets
+        * updated the skin.css file to remove unused styles
+        * updated the structure.css file to remove unused styles
+	* [wiki] removed the textboxlist images
+    * [wiki] removed the textboxlist stylesheets
+    * updated the main logo files
+* updated the titan skin
+	* [wiki] removed the textboxlist images
+    * [wiki] removed the textboxlist stylesheets
+* updated the redmond skin
+    * [login] updated the nova small logo
+    * [login] updated the stylesheets
+        * updated the skin.css file to remove unused styles
+        * updated the structure.css file to remove unused styles
+	* [wiki] removed the textboxlist images
+    * [wiki] removed the textboxlist stylesheets
+* updated jquery to version 1.4.1
+* updated jquery ui to version 1.8rc1
+* updated the head include files to pull in the jquery.ui.widget file which is now required
+* updated the admin's head include file to set some depencies for the ui widgets
+* updated the language files
+    * [install\_lang] added key _update\_required_
+    * [install\_lang] updated key _update\_outofdate\_database_ to change plurality of 'links' to 'link'
+    * [base\_lang] added key _actions\_run_
+* updated the update template to not have a copyright statement
+* updated the install template to not have a copyright statement
+* updated the update versions array
+* updated the manage wiki categories page to allow creating a description
+* updated the wiki head include to pull in the qtip plugin
+* updated the wiki head include to not pull the textboxlist plugin
+* updated the wiki page creation to use a different manner of selecting categories
+* updated the wiki categories to handle a description as well
+* updated the jquery ui images to the base theme
+* updated the jquery ui theme stylesheet to the base theme
+* updated the version history to use the Markdown parser
+* updated the version history accordion to be collapsible
+* removed old jquery ui files (version 1.8 uses a new naming scheme for the .js files)
+* removed test update file
+* removed the version.xml file
+* fixed bug where the update panel wasn't showing the proper information at the right times (#71)
+* fixed bug where viewing a wiki page or draft with fewer than 2 categories wouldn't display the category
+* fixed bug with position sliders not updating the proper item (#72)"),
+	array(
+		'version' => '0.9.5',
+		'version_major' => '0',
+		'version_minor' => '9',
+		'version_update' => '5',
+		'version_date' => now(),
+		'version_launch'	=> 'Nova 0.9.5 is an update to the beta release of the next generation RPG management software from Anodyne Productions.',
+		'version_changes'	=> ""),
 );
 
 $tour = array(
