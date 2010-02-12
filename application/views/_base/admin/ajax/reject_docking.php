ac5d51176263118f07458a7dc410d365fb7e1733
anodyne <davidv@anodyne-productions.com>
2010-02-12 11:04:02 -0500

    added the acceptance and rejection ajax view files

---
+++ application/views/_base/admin/ajax/reject_docking.php
@@ -0,0 +1,26 @@
+<?php echo text_output($header, 'h2');?>
+
+<?php echo text_output($text);?>
+
+<?php echo form_open($form);?>
+	<table class="table100">
+		<tbody>
+			<tr>
+				<td class="cell-label"><?php echo $label['email'];?></td>
+				<td class="cell-spacer"></td>
+				<td><?php echo form_textarea($values['email']);?></td>
+			</tr>
+			
+			<?php echo table_row_spacer(3, 15);?>
+			
+			<tr>
+				<td colspan="2"></td>
+				<td>
+					<?php echo form_hidden('id', $id);?>
+					<?php echo form_hidden('action', 'reject');?>
+					<?php echo form_button($inputs['submit']);?>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+<?php echo form_close();?><br />
