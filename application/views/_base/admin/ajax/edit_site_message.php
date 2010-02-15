50de18ec65f1c9f00cf3b5fb7694fb2ef479a801
anodyne <davidv@anodyne-productions.com>
2010-02-15 10:09:00 -0500

    fixed bug where editing a site message would always show the type as page title (#74)

--- application/views/_base/admin/ajax/edit_site_message.php
+++ application/views/_base/admin/ajax/edit_site_message.php
@@ -1,5 +1,7 @@
 <?php echo text_output($header, 'h2');?>
 
+<?php echo $text;?><br /><br />
+
 <?php echo form_open('site/messages/edit');?>
 	<table class="table100">
 		<tbody>
@@ -22,7 +24,7 @@
 			<tr>
 				<td class="cell-label"><?php echo $label['type'];?></td>
 				<td class="cell-spacer"></td>
-				<td><?php echo form_dropdown('message_type', $type, '', 'class="hud"');?></td>
+				<td><?php echo form_dropdown('message_type', $type, $value['type'], 'class="hud"');?></td>
 			</tr>
 			
 			<?php echo table_row_spacer(3, 10);?>
