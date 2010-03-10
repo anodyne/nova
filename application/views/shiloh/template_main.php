8e52a0ca943dcb9b2b7be8b709eede6b612c7ae5
anodyne <davidv@anodyne-productions.com>
2010-03-10 15:20:39 -0500

    updated the shiloh skin

--- application/views/shiloh/template_main.php
+++ application/views/shiloh/template_main.php
@@ -62,6 +62,15 @@ echo "<?xml version='1.0' encoding='UTF-8'?>\r\n";
 		<!-- JAVASCRIPT FILES -->
 		<?php include_once($this->config->item('include_head_main')); ?>
 		
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url() . APPFOLDER;?>/assets/js/css/uniform.default.css" />
+		
+		<script type="text/javascript" src="<?php echo base_url() . APPFOLDER;?>/assets/js/jquery.uniform.js"></script>
+		<script type="text/javascript">
+			$(document).ready(function(){
+				$('select, input:checkbox, input:radio').uniform();
+			});
+		</script>
+		
 		<?php echo $javascript;?>
 	</head>
 	<body>
