e71529f301a0318c0198bae259f9139c26bdca15
anodyne <davidv@anodyne-productions.com>
2010-03-15 10:49:14 -0400

    updated the archive model with methods for pulling characters, positions and departments

--- application/models/base/archive_model_base.php
+++ application/models/base/archive_model_base.php
@@ -34,6 +34,51 @@ class Archive_model_base extends Model {
 		return $query;
 	}
 	
+	function get_characters($type = '')
+	{
+		$array = array();
+		
+		$dStatement = "SELECT deptid, deptName FROM sms_departments WHERE deptDisplay = 'y' ORDER BY deptOrder ASC";
+		
+		$pStatement = "SELECT positionid, positionName, positionDept FROM sms_positions WHERE positionDept = ? AND ";
+		$pStatement.= "positionDisplay = 'y' ORDER BY positionOrder ASC";
+		
+		$cStatement = "SELECT c.*, r.* FROM sms_crew AS c, sms_ranks AS r WHERE c.crewType = ? AND (c.positionid = ? OR c.positionid2 = ?) ";
+		$cStatement.= "AND c.rankid = r.rankid ORDER BY c.rankid ASC";
+		
+		$dQuery = $this->db->query($dStatement);
+		
+		if ($dQuery->num_rows() > 0)
+		{
+			foreach ($dQuery->result() as $d)
+			{
+				$array[$d->deptid]['dept'] = $d->deptName;
+				
+				$pQuery = $this->db->query($pStatement, array($d->deptid));
+				
+				if ($pQuery->num_rows() > 0)
+				{
+					foreach ($pQuery->result() as $p)
+					{
+						$array[$d->deptid]['positions'][$p->positionid]['position'] = $p->positionName;
+						
+						$cQuery = $this->db->query($cStatement, array($type, $p->positionid, $p->positionid));
+						
+						if ($cQuery->num_rows() > 0)
+						{
+							foreach ($cQuery->result() as $c)
+							{
+								$array[$d->deptid]['positions'][$p->positionid]['characters'][$c->crewid]['name'] = $c->rankName .' '. $c->firstName .' '. $c->lastName;
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		return $array;
+	}
+	
 	function get_db_entry($id = '')
 	{
 		$query = $this->db->query("SELECT * FROM sms_database WHERE dbid = '$id' LIMIT 1");
@@ -44,6 +89,20 @@ class Archive_model_base extends Model {
 	function get_deck_listing()
 	{
 		$query = $this->db->query('SELECT * FROM sms_tour_decks');
+		
+		return $query;
+	}
+	
+	function get_departments()
+	{
+		$query = $this->db->query('SELECT * FROM sms_departments ORDER BY deptOrder ASC');
+		
+		return $query;
+	}
+	
+	function get_positions($dept = '1')
+	{
+		$query = $this->db->query('SELECT * FROM sms_positions WHERE positionDept = ? ORDER BY positionOrder ASC', array($dept));
 		
 		return $query;
 	}
