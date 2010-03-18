3cf85834fb14e0189a33bf0f4d394f4ec8552393
anodyne <davidv@anodyne-productions.com>
2010-03-18 12:27:23 -0400

    updated the missions model to add methods for working with mission groups

--- application/models/base/missions_model_base.php
+++ application/models/base/missions_model_base.php
@@ -27,6 +27,16 @@ class Missions_model_base extends Model {
 	|---------------------------------------------------------------
 	*/
 	
+	function get_all_mission_groups()
+	{
+		$this->db->from('mission_groups');
+		$this->db->order_by('misgroup_order', 'asc');
+		
+		$query = $this->db->get();
+		
+		return $query;
+	}
+	
 	function get_all_missions($status = '')
 	{
 		$this->db->from('missions');
@@ -71,6 +81,53 @@ class Missions_model_base extends Model {
 		return $row;
 	}
 	
+	function get_mission_group($id = '', $return = '')
+	{
+		$query = $this->db->get_where('mission_groups', array('misgroup_id' => $id));
+		
+		$row = ($query->num_rows() > 0) ? $query->row() : FALSE;
+		
+		if (!empty($return) && $row !== FALSE)
+		{
+			if (!is_array($return))
+			{
+				return $row->$return;
+			}
+			else
+			{
+				$array = array();
+				
+				foreach ($return as $r)
+				{
+					$array[$r] = $row->$r;
+				}
+				
+				return $array;
+			}
+		}
+		
+		return $row;
+	}
+	
+	function get_mission_where($where = array(), $display = 'count')
+	{
+		$this->db->from('missions');
+		
+		foreach ($where as $key => $value)
+		{
+			$this->db->where($key, $value);
+		}
+		
+		$query = $this->db->get();
+		
+		if ($display == 'count')
+		{
+			return $query->num_rows();
+		}
+		
+		return $query;
+	}
+	
 	/*
 	|---------------------------------------------------------------
 	| CREATE METHODS
@@ -82,6 +139,15 @@ class Missions_model_base extends Model {
 		$query = $this->db->insert('missions', $data);
 		
 		$this->dbutil->optimize_table('missions');
+		
+		return $query;
+	}
+	
+	function add_mission_group($data = '')
+	{
+		$query = $this->db->insert('mission_groups', $data);
+		
+		$this->dbutil->optimize_table('mission_groups');
 		
 		return $query;
 	}
@@ -102,6 +168,16 @@ class Missions_model_base extends Model {
 		return $query;
 	}
 	
+	function update_mission_group($id = '', $data = '')
+	{
+		$this->db->where('misgroup_id', $id);
+		$query = $this->db->update('mission_groups', $data);
+		
+		$this->dbutil->optimize_table('mission_groups');
+		
+		return $query;
+	}
+	
 	/*
 	|---------------------------------------------------------------
 	| DELETE METHODS
@@ -113,6 +189,15 @@ class Missions_model_base extends Model {
 		$query = $this->db->delete('missions', array('mission_id' => $id));
 		
 		$this->dbutil->optimize_table('missions');
+		
+		return $query;
+	}
+	
+	function delete_mission_group($id = '')
+	{
+		$query = $this->db->delete('mission_groups', array('misgroup_id' => $id));
+		
+		$this->dbutil->optimize_table('mission_groups');
 		
 		return $query;
 	}
