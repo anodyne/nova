ead0c412382b088df1e8bab74ca8c149dfdbcd68
anodyne <davidv@anodyne-productions.com>
2010-04-16 08:29:29 -0400

    added the 1.0.2 update file

---
+++ application/assets/update/update_101.php
@@ -0,0 +1,192 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+|---------------------------------------------------------------
+| UPDATE - 1.0.0 => 1.0.1
+|---------------------------------------------------------------
+*/
+
+/*
+|---------------------------------------------------------------
+| VERSION INFO FOR THE DATABASE
+|---------------------------------------------------------------
+*/
+
+$system_versions = array(
+	'version'			=> '1.0.1',
+	'version_major'		=> 1,
+	'version_minor'		=> 0,
+	'version_update'	=> 1,
+	'version_date'		=> 1271393940,
+	'version_launch'	=> 'Nova 1.0.1 is a maintenance release that fixes two important issues with Nova 1.0. The release fixes a bug where the upgrade process did not create a necessary field in the missions table as well as two issues with installations oh PHP4 servers. This update is recommended for all users who have upgraded from SMS and/or are running on a PHP4 server.',
+	'version_changes'	=> ""
+);
+
+$system_info = array(
+	'sys_last_update'		=> now(),
+	'sys_version_major'		=> 1,
+	'sys_version_minor'		=> 0,
+	'sys_version_update'	=> 1
+);
+
+/*
+|---------------------------------------------------------------
+| TABLES TO ADD
+|
+| $add_tables = array(
+|	'table_name' => array(
+|		'id' => 'table_id',
+|		'fields' => 'fields_table_name')
+| );
+|
+| $fields_table_name = array(
+|	'table_id' => array(
+|		'type' => 'INT',
+|		'constraint' => 6,
+|		'auto_increment' => TRUE),
+|	'table_field_1' => array(
+|		'type' => 'VARCHAR',
+|		'constraint' => 255,
+|		'default' => ''),
+|	'table_field_2' => array(
+|		'type' => 'INT',
+|		'constraint' => 4,
+|		'default' => '99')
+| );
+|---------------------------------------------------------------
+*/
+
+if (isset($add_tables))
+{
+	foreach ($add_table as $key => $value)
+	{
+		$this->dbforge->add_field($$value['fields']);
+		$this->dbforge->add_key($value['id'], TRUE);
+		$this->dbforge->create_table($key, TRUE);
+	}
+}
+
+/*
+|---------------------------------------------------------------
+| TABLES TO DROP
+|
+| $drop_tables = array('table_name');
+|---------------------------------------------------------------
+*/
+
+if (isset($drop_tables))
+{
+	foreach ($drop_tables as $value)
+	{
+		$this->dbforge->drop_table($value);
+	}
+}
+
+/*
+|---------------------------------------------------------------
+| TABLES TO RENAME
+|
+| $rename_tables = array('old_table_name' => 'new_table_name');
+|---------------------------------------------------------------
+*/
+
+if (isset($rename_tables))
+{
+	foreach ($rename_tables as $key => $value)
+	{
+		$this->dbforge->rename_table($key, $value);
+	}
+}
+
+/*
+|---------------------------------------------------------------
+| COLUMNS TO ADD
+|
+| $add_column = array(
+|	'table_name' => array(
+|		'field_name_1' => array('type' => 'TEXT'),
+|		'field_name_2' => array(
+|			'type' => 'VARCHAR',
+|			'constraint' => 100)
+|	)
+| );
+|---------------------------------------------------------------
+*/
+
+$add_column = array(
+	'catalogue_ranks' => array(
+		'rankcat_genre' => array(
+			'type' => 'VARCHAR',
+			'constraint' => 10,
+			'default' => GENRE)
+	)
+);
+
+if (!$this->db->field_exists('mission_group', 'missions'))
+{
+	$add_column['missions'] = array(
+		'mission_group' => array(
+			'type' => 'INT',
+			'constraint' => 5)
+	);
+}
+
+if (isset($add_column))
+{
+	foreach ($add_column as $key => $value)
+	{
+		$this->dbforge->add_column($key, $value);
+	}
+}
+
+/*
+|---------------------------------------------------------------
+| COLUMNS TO MODIFY
+|
+| $modify_column = array(
+|	'table_name' => array(
+|		'old_field_name' => array(
+|			'name' => 'new_field_name',
+|			'type' => 'TEXT')
+|	)
+| );
+|---------------------------------------------------------------
+*/
+
+if (isset($modify_column))
+{
+	foreach ($modify_column as $key => $value)
+	{
+		$this->dbforge->modify_column($key, $value);
+	}
+}
+
+/*
+|---------------------------------------------------------------
+| COLUMNS TO DROP
+|
+| $drop_column = array(
+|	'table_name' => array('field_name')
+| );
+|---------------------------------------------------------------
+*/
+
+if (isset($drop_column))
+{
+	foreach ($drop_column as $key => $value)
+	{
+		$this->dbforge->drop_column($key, $value[0]);
+	}
+}
+
+/*
+|---------------------------------------------------------------
+| DATA TO INSERT/UPDATE/DELETE
+|---------------------------------------------------------------
+*/
+
+/* add system version info */
+$this->load->model('system_model', 'sys');
+$this->sys->add_system_version($system_versions);
+
+/* End of file update_100.php */
+/* Location: ./application/assets/update/update_100.php */
