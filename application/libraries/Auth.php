555af5b0cfa96ebb3d486b28d2fb417714aa2e62
anodyne <davidv@anodyne-productions.com>
2010-04-23 12:54:08 -0400

    added some debugging code to help track down the remember me bug

--- application/libraries/Auth.php
+++ application/libraries/Auth.php
@@ -5,11 +5,12 @@
 |---------------------------------------------------------------
 |
 | File: libraries/Auth.php
-| System Version: 1.0.2
+| System Version: 1.0.3
 |
 | Changes: check to see if a user is pending and if they are,
 |	don't allow them to log in; attmpted fix for the issue with
-|	always being locked out of an account
+|	always being locked out of an account; added some debugging
+|	code to help track down the remember me bug
 |
 | Library for all things authentication
 |
@@ -145,17 +146,18 @@ class Auth {
 			
 			if ($auto !== FALSE)
 			{ /* if the autologin was successful */
+				log_message('error', 'AUTOLOGIN DEBUG - Autologin attempt was successful');
 				return TRUE;
 			}
+			
+			log_message('error', 'AUTOLOGIN DEBUG - Autologin attempt was not successful');
 			
 			if ($redirect === TRUE)
 			{
 				redirect('login/index/error/1');
 			}
-			else
-			{
-				return FALSE;
-			}
+			
+			return FALSE;
 		}
 		
 		return TRUE;
@@ -412,6 +414,8 @@ class Auth {
 		
 		if ($cookie !== FALSE)
 		{
+			log_message('error', 'AUTOLOGIN DEBUG - Attempting autologin for '. $cookie['email'] .' with password hash '. $cookie['password']);
+			
 			$login = $this->login($cookie['email'], $cookie['password']);
 			
 			return $login;
