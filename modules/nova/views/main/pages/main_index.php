a973939f56340a2214a24a4d9a19eba16446ef38
anodyne <davidv@anodyne-productions.com>
2010-05-26 16:22:04 -0400

    updated the view files to reflect some of the model changes

--- modules/nova/views/main/pages/main_index.php
+++ modules/nova/views/main/pages/main_index.php
@@ -7,12 +7,12 @@
 	<h2 class="page-subhead"><?php echo ucwords(__('global.sim').' '.__('global.news'));?></h2>
 	
 	<?php foreach ($news as $n): ?>
-		<h4><?php echo html::anchor('main/viewnews/'.$n->news_id, '&raquo; '.$n->news_title);?></h4>
+		<h4><?php echo html::anchor('main/viewnews/'.$n->id, '&raquo; '.$n->title);?></h4>
 		<p class="subtle fontSmall bold">
-			<?php echo ucfirst(__('action.posted')).' '.Utility::print_date($n->news_date);?>
-			<?php echo __('label.by').' '.Utility::print_character_name($n->news_author_character);?>
-			<?php echo __('label.in').' '.$n->newscat_name;?>
+			<?php echo ucfirst(__('action.posted')).' '.Utility::print_date($n->date);?>
+			<?php //echo __('label.by').' '.Utility::print_character_name($n->author_character);?>
+			<?php echo __('label.in').' '.$n->category->name;?>
 		</p>
-		<p><?php echo nl2br($n->news_content);?></p>
+		<p><?php echo nl2br($n->content);?></p>
 	<?php endforeach;?>
 <?php endif;?>
