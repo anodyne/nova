72946628199400f57f79c3022d31856efebf2585
anodyne <davidv@anodyne-productions.com>
2010-06-07 16:24:32 -0400

    shhh ... adding widgets to nova 2

---
+++ modules/nova/views/_common/widgets/mission_info.php
@@ -0,0 +1,27 @@
+<?php
+
+$missions = Jelly::select('mission')
+	->where('status', '=', 'current')
+	->execute();
+
+if (count($missions) > 0):
+	foreach ($missions as $m):
+	
+?>
+
+		<h4><?php echo html::anchor('sim/mission/'.$m->id, $m->title);?></h4>
+		
+		<?php if (!empty($m->group->name)): ?>
+			<p class="subtle fontSmall bold">
+				<?php echo ucfirst(__('label.in')).' '.$m->group->name;?>
+			</p>
+		<?php endif;?>
+		
+		<p><?php echo nl2br($m->desc);?></p>
+		
+<?php
+
+	endforeach;	
+endif;
+
+?>
---
+++ modules/nova/views/_common/widgets/sim_news.php
@@ -0,0 +1,30 @@
+<?php
+
+$news = Jelly::select('news')
+	->where('status', '=', 'activated')
+	->order_by('date', 'desc')
+	->limit(5);
+	
+(!Auth::is_logged_in()) ? $news->where('private', '=', 'n') : FALSE;
+
+$news = $news->execute();
+
+if (count($news) > 0):
+	foreach ($news as $n):
+	
+?>
+
+		<h4><?php echo html::anchor('main/viewnews/'.$n->id, $n->title);?></h4>
+		<p class="subtle fontSmall bold">
+			<?php echo ucfirst(__('action.posted')).' '.Utility::print_date($n->date);?>
+			<?php echo $n->author_character->print_name();?>
+			<?php echo __('label.in').' '.$n->category->name;?>
+		</p>
+		<p><?php echo nl2br($n->content);?></p>
+		
+<?php
+
+	endforeach;	
+endif;
+
+?>
