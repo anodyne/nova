14cbbb7fdca23fb6f91290471daed404e734a2f6
anodyne <davidv@anodyne-productions.com>
2010-06-09 08:46:35 -0400

    updated the cache module to kohana 3.0.6.2

--- modules/cache/config/cache.php
+++ modules/cache/config/cache.php
@@ -1,15 +1,25 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 return array
 (
-	'default'  => array
-	(
-		'driver'             => 'file',
-		'cache_dir'          => APPPATH.'cache/.kohana_cache',
-		'default_expire'     => 3600,
-	),
 	'memcache' => array
 	(
 		'driver'             => 'memcache',
+		'default_expire'     => 3600,
+		'compression'        => FALSE,              // Use Zlib compression (can cause issues with integers)
+		'servers'            => array
+		(
+			array
+			(
+				'host'             => 'localhost',  // Memcache Server
+				'port'             => 11211,        // Memcache port number
+				'persistent'       => FALSE,        // Persistent connection
+			),
+		),
+		'default_expire'     => 3600,
+	),
+	'memcachetag' => array
+	(
+		'driver'             => 'memcachetag',
 		'default_expire'     => 3600,
 		'compression'        => FALSE,              // Use Zlib compression (can cause issues with integers)
 		'servers'            => array
@@ -36,6 +46,10 @@ return array
 		'schema'             => 'CREATE TABLE caches(id VARCHAR(127) PRIMARY KEY, tags VARCHAR(255), expiration INTEGER, cache TEXT)',
 		'default_expire'     => 3600,
 	),
+	'eaccelerator'           => array
+	(
+		'driver'             => 'eaccelerator',
+	),
 	'xcache'   => array
 	(
 		'driver'             => 'xcache',
@@ -44,7 +58,7 @@ return array
 		'file'    => array
 	(
 		'driver'             => 'file',
-		'cache_dir'          => 'cache/.kohana_cache',
+		'cache_dir'          => APPPATH.'cache/.kohana_cache',
 		'default_expire'     => 3600,
 	)
 );
