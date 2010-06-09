00e1164641eab4bb702ae27298dde6f8fb1545d9
anodyne <davidv@anodyne-productions.com>
2010-06-09 08:47:43 -0400

    updated the core to kohana 3.0.6.2

--- system/classes/kohana/request.php
+++ system/classes/kohana/request.php
@@ -792,6 +792,7 @@ class Kohana_Request {
 	 * ----------|-----------|------------------------------------|--------------
 	 * `boolean` | inline    | Display inline instead of download | `FALSE`
 	 * `string`  | mime_type | Manual mime type                   | Automatic
+	 * `boolean` | delete    | Delete the file after sending      | `FALSE`
 	 *
 	 * Download a file that already exists:
 	 *
@@ -827,6 +828,9 @@ class Kohana_Request {
 			{
 				throw new Kohana_Exception('Download name must be provided for streaming files');
 			}
+
+			// Temporary files will automatically be deleted
+			$options['delete'] = FALSE;
 
 			if ( ! isset($mime))
 			{
@@ -916,6 +920,31 @@ class Kohana_Request {
 		// Close the file
 		fclose($file);
 
+		if ( ! empty($options['delete']))
+		{
+			try
+			{
+				// Attempt to remove the file
+				unlink($filename);
+			}
+			catch (Exception $e)
+			{
+				// Create a text version of the exception
+				$error = Kohana::exception_text($e);
+
+				if (is_object(Kohana::$log))
+				{
+					// Add this exception to the log
+					Kohana::$log->add(Kohana::ERROR, $error);
+
+					// Make sure the logs are written
+					Kohana::$log->write();
+				}
+
+				// Do NOT display the exception, it will corrupt the output!
+			}
+		}
+
 		// Stop execution
 		exit;
 	}
@@ -956,7 +985,7 @@ class Kohana_Request {
 			// Set the benchmark name
 			$benchmark = '"'.$this->uri.'"';
 
-			if ($this !== Request::$instance)
+			if ($this !== Request::$instance AND Request::$current)
 			{
 				// Add the parent request uri
 				$benchmark .= ' « "'.Request::$current->uri.'"';
