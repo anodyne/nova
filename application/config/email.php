168817cad08c3241fdae6d1a032406f2acc1da15
anodyne <davidv@anodyne-productions.com>
2010-06-16 15:05:55 -0400

    added a config file for email options

---
+++ application/config/email.php
@@ -0,0 +1,27 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+return array
+(
+	/**
+	 * The protocol to use for sending email messages. The default is to use
+	 * PHP's built-in mail function but you can also use sendmail or smtp.
+	 * If you choose to use sendmail or SMTP email, you must also provide the
+	 * necessary information below.
+	 */
+	'type' => 'mail',
+	
+	/**
+	 * SMTP server information. This is needed to connect to the SMTP server to
+	 * send email.
+	 */
+	'smtp_server' => 'smtp.example.com',
+	'smtp_port' => '25',
+	'smtp_username' => 'username',
+	'smtp_password' => 'password',
+	
+	/**
+	 * Sendmail information. This is needed to connect to sendmail to send email
+	 * through the script.
+	 */
+	'sendmail_path' => '/usr/sbin/sendmail -bs',
+);
