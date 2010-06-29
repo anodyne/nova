79234ab2160e8bbd5faf7564735a8ee71848500e
anodyne <davidv@anodyne-productions.com>
2010-06-29 10:59:49 -0400

    added some new model builder files

---
+++ modules/nova/core/classes/model/builder/cataloguerank.php
@@ -0,0 +1,23 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Rank Catalogue Builder Model
+ *
+ * @package		Nova
+ * @category	Model Builders
+ * @author		Anodyne Productions
+ */
+ 
+class Model_Builder_Cataloguerank extends Jelly_Builder
+{
+	/**
+	 * Creates a where statement for figuring out the system default rank set.
+	 *
+	 *     $setting = Jelly::select('cataloguerank')->defaultrank()->load();
+	 *
+	 * @return	object Jelly_Builder object
+	 */
+	public function defaultrank()
+	{
+		return $this->where('genre', '=', Kohana::config('nova.genre'))->where('default', '=', 'y');
+	}
+}
---
+++ modules/nova/core/classes/model/builder/catalogueskinsec.php
@@ -0,0 +1,23 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Skin Section Catalogue Builder Model
+ *
+ * @package		Nova
+ * @category	Model Builders
+ * @author		Anodyne Productions
+ */
+ 
+class Model_Builder_Catalogueskinsec extends Jelly_Builder
+{
+	/**
+	 * Creates a where statement for figuring out the system default skin for a section.
+	 *
+	 *     $setting = Jelly::select('catalogueskinsec')->defaultskin('main')->load();
+	 *
+	 * @return	object Jelly_Builder object
+	 */
+	public function defaultskin($section)
+	{
+		return $this->where('status', '=', 'active')->where('default', '=', 'y')->where('section', '=', $section);
+	}
+}
