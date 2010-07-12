b55358ce82dcb4cd6fb39e8a47d9d03197e7de7c
agentphoenix <david.vanscott@gmail.com>
2010-07-12 14:59:40 -0400

    updates to the awards received model

--- modules/nova/core/classes/model/awardrec.php
+++ modules/nova/core/classes/model/awardrec.php
@@ -21,11 +21,13 @@ class Model_Awardrec extends Jelly_Model
 			'id' => new Field_Primary(array(
 				'column' => 'awardrec_id'
 			)),
-			'user' => new Field_Integer(array(
-				'column' => 'awardrec_user'
+			'user' => new Field_BelongsTo(array(
+				'column' => 'awardrec_user',
+				'foreign' => 'user'
 			)),
-			'character' => new Field_Integer(array(
-				'column' => 'awardrec_character'
+			'character' => new Field_BelongsTo(array(
+				'column' => 'awardrec_character',
+				'foreign' => 'character'
 			)),
 			'nominated' => new Field_Integer(array(
 				'column' => 'awardrec_nominated_by',
