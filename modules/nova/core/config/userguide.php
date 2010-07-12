751bf3d9088cc079309882bf0f66cfd74605fd07
agentphoenix <david.vanscott@gmail.com>
2010-07-12 14:58:55 -0400

    updated nova's core module override of the userguide config file to set the default page

--- modules/nova/core/config/userguide.php
+++ modules/nova/core/config/userguide.php
@@ -2,6 +2,9 @@
 
 return array
 (
+	// Default userguide page.
+	'default_page' => 'basics.about',
+	
 	// Enable these packages in the API browser.  TRUE for all packages, or a string of comma seperated packages, using 'None' for a class with no @package
 	// Example: 'api_packages' => 'Kohana,Kohana/Database,Kohana/ORM,None',
 	//'api_packages' => TRUE,
