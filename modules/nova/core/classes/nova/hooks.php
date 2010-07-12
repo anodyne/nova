5e453663c1b8ebe50590ef57c76f5d731df6a588
agentphoenix <david.vanscott@gmail.com>
2010-07-12 14:59:27 -0400

    fixed some bugs in the maintenance hook

--- modules/nova/core/classes/nova/hooks.php
+++ modules/nova/core/classes/nova/hooks.php
@@ -47,26 +47,30 @@ abstract class Nova_Hooks
 	
 	public static function maintenance()
 	{
-		// get an instance of the request object
-		$request = Request::instance();
-		
-		// get an instance of the session object
-		$session = Session::instance();
-		
-		// figure out which controllers to ignore
-		$ignore = array('install', 'update', 'upgrade', 'upgradeajax', 'login');
-		
-		if (!in_array($request->controller, $ignore))
+		// if the config file isn't set
+		if (file_exists(APPPATH.'config/database'.EXT) && Utility::install_status())
 		{
-			// get the maintenance setting
-			$maint = Jelly::select('setting')->key('maintenance')->load()->value;
+			// get an instance of the request object
+			$request = Request::instance();
 			
-			if ($maint == 'on' && $request->controller != 'login')
+			// get an instance of the session object
+			$session = Session::instance();
+			
+			// figure out which controllers to ignore
+			$ignore = array('install', 'update', 'upgrade', 'upgradeajax', 'login');
+			
+			if (!in_array($request->controller, $ignore))
 			{
-				if (!Auth::is_type('sysadmin', $session->get('userid')))
+				// get the maintenance setting
+				$maint = Jelly::select('setting')->key('maintenance')->load()->value;
+				
+				if ($maint == 'on' && $request->controller != 'login')
 				{
-					// redirect to the login page
-					$request->redirect('login/maintenance');
+					if (!Auth::is_type('sysadmin', $session->get('userid')))
+					{
+						// redirect to the login page
+						$request->redirect('login/maintenance');
+					}
 				}
 			}
 		}
