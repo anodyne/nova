d39738926a7d365abc9c73b5adba0c1de3b5d936
agentphoenix <david.vanscott@gmail.com>
2010-08-16 10:49:32 -0400

    added the system components model

---
+++ modules/nova/core/classes/model/systemcomponent.php
@@ -0,0 +1,38 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * System Versions Model
+ *
+ * @package		Nova
+ * @category	Models
+ * @author		Anodyne Productions
+ */
+ 
+class Model_Systemcomponent extends Jelly_Model
+{
+	/**
+	 * Initialize the model with Jelly_Meta data
+	 *
+	 * @return	void
+	 */
+	public static function initialize(Jelly_Meta $meta)
+	{
+		$meta->table('system_components');
+		$meta->fields(array(
+			'id' => Jelly::field('primary', array(
+				'column' => 'comp_id'
+			)),
+			'name' => Jelly::field('string', array(
+				'column' => 'comp_name'
+			)),
+			'version' => Jelly::field('string', array(
+				'column' => 'comp_version'
+			)),
+			'url' => Jelly::field('string', array(
+				'column' => 'comp_url'
+			)),
+			'desc' => Jelly::field('text', array(
+				'column' => 'comp_desc'
+			)),
+		));
+	}
+}
