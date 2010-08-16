37df61c765a60235721f551cd6a331ecb2cab143
agentphoenix <david.vanscott@gmail.com>
2010-08-16 10:51:40 -0400

    removed the jelly 0.9.6 module

--- modules/nova/jelly/classes/jelly/builder.php
+++
@@ -1,6 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Jelly_Builder extends Jelly_Builder_Core
-{
-
-}
--- modules/nova/jelly/classes/jelly/meta.php
+++
@@ -1,6 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Jelly_Meta extends Jelly_Meta_Core
-{
-
-}
