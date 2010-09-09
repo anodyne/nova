c690f224d5c8bc92f69c162ab46076f17631b3c8
agentphoenix <david.vanscott@gmail.com>
2010-09-09 10:29:19 -0400

    adding the dbforge to the user guide content

--- modules/nova/core/config/userguide.php
+++ modules/nova/core/config/userguide.php
@@ -8,7 +8,7 @@ return array
 	// Enable these packages in the API browser.  TRUE for all packages, or a string of comma seperated packages, using 'None' for a class with no @package
 	// Example: 'api_packages' => 'Kohana,Kohana/Database,Kohana/ORM,None',
 	//'api_packages' => TRUE,
-	'api_packages' => 'Kohana,Jelly,Nova',
+	'api_packages' => 'Kohana,Jelly,Nova,DBForge',
 	
 	// Class prefixes to ignore
 	'api_prefix_ignore' => 'kohana_,nova_',
