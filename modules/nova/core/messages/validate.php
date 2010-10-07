00990433eb8ab747b4d9d05889a1ae5bc5366b21
agentphoenix <david.vanscott@gmail.com>
2010-10-07 09:47:09 -0400

    overriding some of kohana's default validation messages

---
+++ modules/nova/core/messages/validate.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	'email'         => ':field must be an email address',
+	'not_empty'     => ':field cannot be empty',
+	'url'           => ':field must be a URL',
+);
