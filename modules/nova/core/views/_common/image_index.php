bdf22ac2ea6edb36a56504dfd4145d82abcf0b5d
agentphoenix <david.vanscott@gmail.com>
2010-10-08 11:12:46 -0400

    added an image to the image index

--- modules/nova/core/views/_common/image_index.php
+++ modules/nova/core/views/_common/image_index.php
@@ -2,12 +2,12 @@
 /**
  * Base Image Index
  *
- * @package		Nova Core
- * @author		David VanScott [davidv@anodyne-productions.com]
- * @version		2.0
+ * @package		Nova
+ * @author		Anodyne Productions
  */
 
 return array(
+	'main.error'		=> 'exclamation-red.png',
 	'main.next'			=> 'next.png',
 	'main.previous'		=> 'previous.png',
 	'main.rss'			=> 'feed.png',
