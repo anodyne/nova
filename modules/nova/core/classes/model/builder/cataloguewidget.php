821488eec4eef9d85757b31c859b597077bd463f
agentphoenix <david.vanscott@gmail.com>
2010-10-11 15:28:57 -0400

    adding the widget catalogue model

---
+++ modules/nova/core/classes/model/builder/cataloguewidget.php
@@ -0,0 +1,30 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Widget Catalogue Builder Model
+ *
+ * @package		Nova
+ * @category	Model Builders
+ * @author		Anodyne Productions
+ */
+ 
+class Model_Builder_Cataloguewidget extends Jelly_Builder {
+	
+	/**
+	 * Overrides the unique key functionality so that the model will understand
+	 * both numeric values for primary keys and string values for the name key.
+	 *
+	 * @param	mixed	value to use as the unique key
+	 * @return	object	the primary key or name key object
+	 */
+	public function unique_key($value)
+	{
+		if (is_numeric($value))
+		{
+			return $this->_meta->primary_key();
+		}
+		else
+		{
+			return $this->_meta->name_key();
+		}
+	}
+}
---
+++ modules/nova/core/classes/model/cataloguewidget.php
@@ -0,0 +1,46 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Widgets Catalogue Model
+ *
+ * @package		Nova
+ * @category	Models
+ * @author		Anodyne Productions
+ */
+ 
+class Model_Cataloguewidget extends Jelly_Model {
+	
+	/**
+	 * Initialize the model with Jelly_Meta data
+	 *
+	 * @return	void
+	 */
+	public static function initialize(Jelly_Meta $meta)
+	{
+		$meta->table('catalogue_widgets');
+		$meta->fields(array(
+			'id' => Jelly::field('primary', array(
+				'column' => 'widget_id'
+			)),
+			'name' => Jelly::field('string', array(
+				'column' => 'widget_name'
+			)),
+			'location' => Jelly::field('string', array(
+				'column' => 'widget_location'
+			)),
+			'page' => Jelly::field('string', array(
+				'column' => 'widget_page'
+			)),
+			'zone' => Jelly::field('integer', array(
+				'column' => 'widget_zone'
+			)),
+			'status' => Jelly::field('enum', array(
+				'column' => 'widget_status',
+				'choices' => array('active', 'inactive', 'development'),
+				'default' => 'active'
+			)),
+			'credits' => Jelly::field('text', array(
+				'column' => 'widget_credits'
+			)),
+		));
+	}
+}
