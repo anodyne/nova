3ee93203e39d6dc0864e1504fb7c9d35250d18bb
agentphoenix <david.vanscott@gmail.com>
2010-11-22 10:45:38 -0500

    added the nested admin/manage controller (not for inclusion in M2, just testing out the HMVC stuff)

---
+++ modules/nova/core/classes/controller/admin/manage.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Manage Controller
+ *
+ * @package		Nova
+ * @category	Controllers
+ * @author		Anodyne Productions
+ */
+
+class Controller_Admin_Manage extends Controller_Nova_Admin_Manage {
+	
+	public function before()
+	{
+		parent::before();
+	}
+}
