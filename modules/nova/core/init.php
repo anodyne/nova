b823d70959376686faf192248962a857afb58e5e
agentphoenix <david.vanscott@gmail.com>
2010-11-22 10:44:54 -0500

    updated the core nova init file to build a test route for the admin/manage controller with HMVC

--- modules/nova/core/init.php
+++ modules/nova/core/init.php
@@ -1,1 +1,8 @@
 <?php defined('SYSPATH') or die('No direct script access.');
+
+Route::set('admin', 'admin/manage(/<action>(/<id>))')
+	->defaults(array( 
+		'directory' => 'admin', 
+		'controller' => 'manage', 
+		'action' => 'index'
+	));
