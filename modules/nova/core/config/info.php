a556431350752e43694afa6c4df8df9cd8ce791c
agentphoenix <david.vanscott@gmail.com>
2010-12-01 12:58:49 -0500

    updated the nova info config file with the right version information

--- modules/nova/core/config/info.php
+++ modules/nova/core/config/info.php
@@ -11,7 +11,7 @@ return array(
 	 */
 	 
 	'app_name' => 'Nova',
-	'app_version_full' => '2.0-m2.1',
+	'app_version_full' => '2.0-m2',
 	'app_version_major' => 2,
 	'app_version_minor' => 0,
 	'app_version_update' => 0,
@@ -21,9 +21,9 @@ return array(
 	 */
 	 
 	'wiki_name' => 'Thresher',
-	'wiki_version_full' => 'Release 1',
+	'wiki_version_full' => 'Release 2',
 	'wiki_version_major' => 1,
-	'wiki_version_minor' => 0,
+	'wiki_version_minor' => 1,
 	'wiki_version_update' => 0,
 	
 	/**
@@ -36,5 +36,5 @@ return array(
 	/**
 	 * System Information
 	 */
-	'app_db_tables' => 60,
+	'app_db_tables' => 61,
 );
