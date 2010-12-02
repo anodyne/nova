a983f80bfa50798246475c0701241e7ad305e04b
agentphoenix <david.vanscott@gmail.com>
2010-12-02 18:28:21 -0500

    added the user model builder

---
+++ modules/nova/core/classes/model/builder/user.php
@@ -0,0 +1,57 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * User Builder Model
+ *
+ * @package		Nova
+ * @category	Model Builders
+ * @author		Anodyne Productions
+ */
+ 
+class Model_Builder_User extends Jelly_Builder {
+	
+	/**
+	 * Calculates the status of the user based on their characters.
+	 *
+	 *     $status = Jelly::query('user', 1)->get_status();
+	 *
+	 * @return	string 	a string of the status of the user (active, inactive, pending)
+	 */
+	public function get_status()
+	{
+		$query = $this->select();
+		
+		if (($query->main_char === null) or ($query->main_char->id == 0))
+		{
+			$status = 'inactive';
+		}
+		else
+		{
+			if (count($query->characters) > 0)
+			{
+				foreach ($query->characters as $c)
+				{
+					$chars[] = $c['status'];
+				}
+				
+				if (in_array('active', $chars))
+				{
+					$status = 'active';
+				}
+				elseif ( ! in_array('active', $chars) and in_array('pending', $chars))
+				{
+					$status = 'pending';
+				}
+				elseif ( ! in_array('active', $chars) and ! in_array('pending', $chars))
+				{
+					$status = 'inactive';
+				}
+			}
+			else
+			{
+				$status = 'inactive';
+			}
+		}
+		
+		return $status;
+	}
+}
