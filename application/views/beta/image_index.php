2118790ecdf10cdca2b9e405d5690559da9a1954
agentphoenix <david.vanscott@gmail.com>
2010-12-02 11:29:26 -0500

    updated the beta skin's image index to not be an empty file

---
+++ application/views/beta/image_index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Skin Image Index
+ *
+ * You can override the system image defaults by creating new array keys:
+ *
+ * $images['main.feed'] = 'rss.png';
+ *
+ * Nova will look for a file called rss.png in your skin, then the override,
+ * then finally in the modules
+ */
+
+return array();
