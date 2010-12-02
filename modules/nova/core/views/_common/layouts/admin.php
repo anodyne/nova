2acdb2f4fc3111875b84d8813fdc56c0b30aa9df
agentphoenix <david.vanscott@gmail.com>
2010-12-02 18:28:13 -0500

    updated the admin layout

--- modules/nova/core/views/_common/layouts/admin.php
+++ modules/nova/core/views/_common/layouts/admin.php
@@ -14,18 +14,44 @@
 		<?php echo html::style(MODFOLDER.'/nova/core/views/_common/css/nova.css');?>
 		<?php echo html::style(APPFOLDER.'/views/'.$skin.'/'.$sec.'/css/main.css');?>
 		
+		<!-- jQUERY UI THEME STYLESHEET -->
+		<?php if (is_file(APPPATH.'views/'.$skin.'/'.$sec.'/css/jquery.ui.theme.css')): ?>
+			<?php $uiTheme = url::base().APPFOLDER.'/views/'.$skin.'/'.$sec.'/css/jquery.ui.theme.css';?>
+		<?php else: ?>
+			<?php $uiTheme = url::base().MODFOLDER.'/assets/css/jquery.ui.theme.css';?>
+		<?php endif;?>
+		
 		<!-- jQUERY UI TABS STYLESHEET -->
 		<?php if (is_file(APPPATH.'views/'.$skin.'/'.$sec.'/css/jquery.ui.tabs.css')): ?>
-			<?php //echo html::style(APPFOLDER.'/views/'.$skin.'/'.$sec.'/css/jquery.ui.tabs.css');?>
+			<?php $uiTabs = url::base().APPFOLDER.'/views/'.$skin.'/'.$sec.'/css/jquery.ui.tabs.css';?>
 		<?php else: ?>
-			<?php //echo html::style(MODFOLDER.'/assets/css/jquery.ui.tabs.css');?>
+			<?php $uiTabs = url::base().MODFOLDER.'/assets/css/jquery.ui.tabs.css';?>
 		<?php endif;?>
 		
 		<!-- JAVASCRIPT -->
 		<?php echo html::script(MODFOLDER.'/assets/js/jquery.js');?>
-		<?php //echo html::script(MODFOLDER.'/assets/js/jquery.ui.core.min.js');?>
-		<?php //echo html::script(MODFOLDER.'/assets/js/jquery.ui.widget.min.js');?>
-		<?php //echo html::script(MODFOLDER.'/assets/js/jquery.ui.tabs.min.js');?>
+		<?php echo html::script(MODFOLDER.'/assets/js/jquery.lazy.js');?>
+		
+		<script type="text/javascript">
+			$(document).ready(function(){
+				$.lazy({
+					src: '<?php echo url::base().MODFOLDER;?>/assets/js/jquery.ui.tabs.min.js',
+					name: 'tabs',
+					dependencies: {
+						js: [
+							'<?php echo url::base().MODFOLDER;?>/assets/js/jquery.ui.core.min.js',
+							'<?php echo url::base().MODFOLDER;?>/assets/js/jquery.ui.widget.min.js'
+						],
+						css: [
+							'<?php echo $uiTheme;?>',
+							'<?php echo $uiTabs;?>'
+						]
+					},
+					cache: true
+				});
+			});
+		</script>
+		
 		<?php echo $javascript;?>
 	</head>
 	<body>
