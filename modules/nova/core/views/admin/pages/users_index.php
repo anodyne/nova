305d8ccaeffa2eae4e138ff33df91c9cc199a30a
agentphoenix <david.vanscott@gmail.com>
2010-12-10 12:56:36 -0500

    updated the users/index view file

--- modules/nova/core/views/admin/pages/users_index.php
+++ modules/nova/core/views/admin/pages/users_index.php
@@ -10,33 +10,95 @@
 	
 	<div id="active">
 		<?php if (isset($users['active'])): ?>
-			<?php foreach ($users['active'] as $u): ?>
-				<p><?php echo $u->name;?></p>
-			<?php endforeach;?>
+			<table class="table100 zebra">
+				<tbody>
+				<?php foreach ($users['active'] as $u): ?>
+					<tr>
+						<td><?php echo $u->name;?></td>
+						<td class="col-100 align-center">
+							<?php echo html::image($images['link']['src'], $images['link']['attr']);?>
+							&nbsp;
+							<?php echo html::image($images['delete']['src'], $images['delete']['attr']);?>
+							&nbsp;
+							<?php echo html::image($images['edit']['src'], $images['edit']['attr']);?>
+						</td>
+					</tr>
+				<?php endforeach;?>
+				</tbody>
+			</table>
+		<?php else: ?>
+			<h3 class="warning"><?php echo ucfirst(__('no :item were found.', array(':item' => __('active').' '.__('users'))));?></h3>
 		<?php endif;?>
 	</div>
 	
 	<div id="inactive">
 		<?php if (isset($users['inactive'])): ?>
-			<?php foreach ($users['inactive'] as $u): ?>
-				<p><?php echo $u->name;?></p>
-			<?php endforeach;?>
+			<table class="table100 zebra">
+				<tbody>
+				<?php foreach ($users['inactive'] as $u): ?>
+					<tr>
+						<td><?php echo $u->name;?></td>
+						<td class="col-100 align-center">
+							<?php echo html::image($images['link']['src'], $images['link']['attr']);?>
+							&nbsp;
+							<?php echo html::image($images['delete']['src'], $images['delete']['attr']);?>
+							&nbsp;
+							<?php echo html::image($images['edit']['src'], $images['edit']['attr']);?>
+						</td>
+					</tr>
+				<?php endforeach;?>
+				</tbody>
+			</table>
+		<?php else: ?>
+			<h3 class="warning"><?php echo ucfirst(__('no :item were found.', array(':item' => __('inactive').' '.__('users'))));?></h3>
 		<?php endif;?>
 	</div>
 	
 	<div id="retired">
 		<?php if (isset($users['retired'])): ?>
-			<?php foreach ($users['retired'] as $u): ?>
-				<p><?php echo $u->name;?></p>
-			<?php endforeach;?>
+			<table class="table100 zebra">
+				<tbody>
+				<?php foreach ($users['retired'] as $u): ?>
+					<tr>
+						<td><?php echo $u->name;?></td>
+						<td class="col-100 align-center">
+							<?php echo html::image($images['link']['src'], $images['link']['attr']);?>
+							&nbsp;
+							<?php echo html::image($images['delete']['src'], $images['delete']['attr']);?>
+							&nbsp;
+							<?php echo html::image($images['edit']['src'], $images['edit']['attr']);?>
+						</td>
+					</tr>
+				<?php endforeach;?>
+				</tbody>
+			</table>
+		<?php else: ?>
+			<h3 class="warning"><?php echo ucfirst(__('no :item were found.', array(':item' => __('retired').' '.__('users'))));?></h3>
 		<?php endif;?>
 	</div>
 	
 	<div id="pending">
 		<?php if (isset($users['pending'])): ?>
-			<?php foreach ($users['pending'] as $u): ?>
-				<p><?php echo $u->name;?></p>
-			<?php endforeach;?>
+			<table class="table100 zebra">
+				<tbody>
+				<?php foreach ($users['pending'] as $u): ?>
+					<tr>
+						<td><?php echo $u->name;?></td>
+						<td class="col-100 align-center">
+							<?php echo html::image($images['approve']['src'], $images['approve']['attr']);?>
+							&nbsp;
+							<?php echo html::image($images['reject']['src'], $images['reject']['attr']);?>
+							&nbsp;
+							<?php echo html::image($images['delete']['src'], $images['delete']['attr']);?>
+							&nbsp;
+							<?php echo html::image($images['edit']['src'], $images['edit']['attr']);?>
+						</td>
+					</tr>
+				<?php endforeach;?>
+				</tbody>
+			</table>
+		<?php else: ?>
+			<h3 class="warning"><?php echo ucfirst(__('no :item were found.', array(':item' => __('pending').' '.__('users'))));?></h3>
 		<?php endif;?>
 	</div>
 </div>
