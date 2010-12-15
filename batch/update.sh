4de35fa3ad36da7ce7e2b5592506896944347ae1
agentphoenix <david.vanscott@gmail.com>
2010-12-15 08:04:56 -0500

    added the update bash script that will automatically generate the changelog and changed files listing

---
+++ batch/update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+old="support-1.1"
+new="1.x"
+
+git diff $old..$new --name-status > CHANGED_FILES
+
+git log $old..$new --pretty=format:"* %s" > CHANGELOG
+
+echo "Updates complete."
