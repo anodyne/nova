1f76d1e24ec86f352d4223acdd77e77aff79def6
agentphoenix <david.vanscott@gmail.com>
2010-12-21 12:30:56 -0500

    added the 1.3 update file

---
+++ application/assets/update/update_130.php
@@ -0,0 +1,182 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+|---------------------------------------------------------------
+| UPDATE - 1.2.x => 1.3
+|---------------------------------------------------------------
+*/
+
+$system_versions	= NULL;
+$system_info		= NULL;
+$add_tables			= NULL;
+$drop_tables		= NULL;
+$rename_tables		= NULL;
+$add_column			= NULL;
+$modify_column		= NULL;
+$drop_column		= NULL;
+
+/*
+|---------------------------------------------------------------
+| VERSION INFO FOR THE DATABASE
+|---------------------------------------------------------------
+*/
+
+$system_versions = array(
+	'version'			=> '1.3.0',
+	'version_major'		=> 1,
+	'version_minor'		=> 3,
+	'version_update'	=> 0,
+	'version_date'		=> 1292889600,
+	'version_launch'	=> "Nova 1.3 is the third major update to Nova 1 and adds new functionality to the system to help admins manage their RPG. [...] More information about these features and a full changelog can be found at AnodyneDocs. This update is recommended for all users.",
+	'version_changes'	=> ""
+);
+
+$system_info = array(
+	'sys_last_update'		=> now(),
+	'sys_version_major'		=> 1,
+	'sys_version_minor'		=> 3,
+	'sys_version_update'	=> 0
+);
+
+/*
+|---------------------------------------------------------------
+| TABLES TO ADD
+|
+| $add_tables = array(
+|	'table_name' => array(
+|		'id' => 'table_id',
+|		'fields' => 'fields_table_name')
+| );
+|
+| $fields_table_name = array(
+|	'table_id' => array(
+|		'type' => 'INT',
+|		'constraint' => 6,
+|		'auto_increment' => TRUE),
+|	'table_field_1' => array(
+|		'type' => 'VARCHAR',
+|		'constraint' => 255,
+|		'default' => ''),
+|	'table_field_2' => array(
+|		'type' => 'INT',
+|		'constraint' => 4,
+|		'default' => '99')
+| );
+|---------------------------------------------------------------
+*/
+
+if (!is_null($add_tables))
+{
+	foreach ($add_tables as $key => $value)
+	{
+		$this->dbforge->add_field($$value['fields']);
+		$this->dbforge->add_key($value['id'], TRUE);
+		$this->dbforge->create_table($key, TRUE);
+	}
+}
+
+/*
+|---------------------------------------------------------------
+| TABLES TO DROP
+|
+| $drop_tables = array('table_name');
+|---------------------------------------------------------------
+*/
+
+if (!is_null($drop_tables))
+{
+	foreach ($drop_tables as $value)
+	{
+		$this->dbforge->drop_table($value);
+	}
+}
+
+/*
+|---------------------------------------------------------------
+| TABLES TO RENAME
+|
+| $rename_tables = array('old_table_name' => 'new_table_name');
+|---------------------------------------------------------------
+*/
+
+if (!is_null($rename_tables))
+{
+	foreach ($rename_tables as $key => $value)
+	{
+		$this->dbforge->rename_table($key, $value);
+	}
+}
+
+/*
+|---------------------------------------------------------------
+| COLUMNS TO ADD
+|
+| $add_column = array(
+|	'table_name' => array(
+|		'field_name_1' => array('type' => 'TEXT'),
+|		'field_name_2' => array(
+|			'type' => 'VARCHAR',
+|			'constraint' => 100)
+|	)
+| );
+|---------------------------------------------------------------
+*/
+
+if (!is_null($add_column))
+{
+	foreach ($add_column as $key => $value)
+	{
+		$this->dbforge->add_column($key, $value);
+	}
+}
+
+/*
+|---------------------------------------------------------------
+| COLUMNS TO MODIFY
+|
+| $modify_column = array(
+|	'table_name' => array(
+|		'old_field_name' => array(
+|			'name' => 'new_field_name',
+|			'type' => 'TEXT')
+|	)
+| );
+|---------------------------------------------------------------
+*/
+
+if (!is_null($modify_column))
+{
+	foreach ($modify_column as $key => $value)
+	{
+		$this->dbforge->modify_column($key, $value);
+	}
+}
+
+/*
+|---------------------------------------------------------------
+| COLUMNS TO DROP
+|
+| $drop_column = array(
+|	'table_name' => array('field_name')
+| );
+|---------------------------------------------------------------
+*/
+
+if (!is_null($drop_column))
+{
+	foreach ($drop_column as $key => $value)
+	{
+		$this->dbforge->drop_column($key, $value[0]);
+	}
+}
+
+/*
+|---------------------------------------------------------------
+| DATA TO INSERT/UPDATE/DELETE
+|---------------------------------------------------------------
+*/
+
+/**
+ * update the jquery version info
+ */
+$this->db->where('comp_name', 'CodeIgniter');
+$this->db->update('system_components', array('comp_version' => '1.7.3'));
