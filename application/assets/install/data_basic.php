7162f2a41adab0f3730fcf04afab6c685c720cab
agentphoenix <david.vanscott@gmail.com>
2010-12-30 19:16:35 -0500

    updating the install and 1.2.2 update file

--- application/assets/install/data_basic.php
+++ application/assets/install/data_basic.php
@@ -2636,11 +2636,12 @@ $system_versions = array(
 		'version_major' => '1',
 		'version_minor' => '2',
 		'version_update' => '2',
-		'version_date' => 1293753600,
+		'version_date' => 1293759000,
 		'version_launch'	=> "Nova 1.2.2 is the second maintenance release for Nova 1.2 and two bugs discovered after the release of Nova 1.2.1. Bugs fixed in this release include a bug where sub departments couldn't be managed from the department management page and a bug with the display of post authors in emails. More information about these features and a full changelog can be found at AnodyneDocs. This update is recommended for all users.",
 		'version_changes'	=> "* added the 1.2.2 update file
 * fixed bug where sub departments couldn't be managed from the department management page
-* fixed bug where post emails sent out didn't display the authors properly"),
+* fixed bug where post emails sent out didn't display the authors properly
+* fixed bug in the 1.1.2 to 1.2 update file that would cause access issues"),
 );
 
 $tour_fields = array(
