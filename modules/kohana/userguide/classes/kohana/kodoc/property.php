ed119ed63a9ffb8e7f85e8a80c9b95825c3cb229
agentphoenix <david.vanscott@gmail.com>
2011-01-17 10:14:31 -0500

    updated the userguide module to version 3.1rc2

--- modules/kohana/userguide/classes/kohana/kodoc/property.php
+++ modules/kohana/userguide/classes/kohana/kodoc/property.php
@@ -51,33 +51,17 @@ class Kohana_Kodoc_Property extends Kodoc {
 
 				if (isset($matches[2]))
 				{
-					$this->description = Markdown($matches[2]);
+					$this->description = $matches[2];
 				}
 			}
 		}
 
 		$this->property = $property;
-		
-		// Show the value of static properties, but only if they are public or we are php 5.3 or higher and can force them to be accessible
-		if ($property->isStatic() AND ($property->isPublic() OR version_compare(PHP_VERSION, '5.3', '>=')))
+
+		if ($property->isStatic())
 		{
-			// Force the property to be accessible
-			if (version_compare(PHP_VERSION, '5.3', '>='))
-			{
-				$property->setAccessible(TRUE);
-			}
-			
-			// Don't debug the entire object, just say what kind of object it is
-			if (is_object($property->getValue($class)))
-			{
-				$this->value = '<pre>object '.get_class($property->getValue($class)).'()</pre>';
-			}
-			else
-			{
-				$this->value = Debug::vars($property->getValue($class));
-			}
+			$this->value = Debug::vars($property->getValue($class));
 		}
-		
 	}
 
 } // End Kodoc_Property
--- modules/kohana/userguide/views/userguide/api/menu.php
+++ modules/kohana/userguide/views/userguide/api/menu.php
@@ -1,16 +1,14 @@
-
-<h2>Modules</h2>
 <ol class="menu">
 <?php foreach ($menu as $package => $categories): ksort($categories); ?>
-<li><span><strong><?php echo $package ?></strong></span>
+<li><strong><?php echo $package ?></strong>
 	<ol>
 	<?php foreach ($categories as $category => $classes): sort($classes); ?>
-		<li><?php if ($category !== 'Base'): ?><span><?php echo $category ?></span>
-			<ol><?php endif ?>
+		<li><?php if ($category !== 'Base'): ?><strong><?php echo $category ?></strong><?php endif ?>
+			<ol>
 			<?php foreach ($classes as $class): ?>
 				<li><?php echo $class ?></li>
 			<?php endforeach ?>
-			<?php if ($category !== 'Base'): ?></ol><?php endif ?>
+			</ol>
 		</li>
 	<?php endforeach ?>
 	</ol>
