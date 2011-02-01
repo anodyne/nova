50b8d714c11ab47a7a3820bb09b446be943b44dd
agentphoenix <david.vanscott@gmail.com>
2011-02-01 15:19:56 -0500

    updated the info constants

--- nova/modules/core/config/info.php
+++ nova/modules/core/config/info.php
@@ -12,14 +12,14 @@ define('WIKI_VERSION',			'Release 2');
 
 define('SMS_UPGRADE_VERSION',	'2.6.9');
 
-//define('VERSION_FEED', APPPATH . 'assets/version.yml');
+//define('VERSION_FEED', MODPATH.'assets/version.yml');
 define('VERSION_FEED', 'http://www.anodyne-productions.com/feeds/version_nova.yml');
 
 /* figure out whether to install the bare essentials or the dev stuff */
 define('APP_DATA_SRC', 'basic');
-define('APP_DATA_DEV', FALSE);
+define('APP_DATA_DEV', false);
 
 /* figure out if the request is an ajax request */
 define('IS_AJAX', isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest');
 
-/* CI_VERSION is available as a constant and is defined in ./core/codeigniter/CodeIgniter.php */
+/* CI_VERSION is available as a constant and is defined in ./nova/codeigniter/codeigniter/CodeIgniter.php */
