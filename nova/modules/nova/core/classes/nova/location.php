975033dae4daa1a0bb2d4d3be3cb16fb94cd92b4
agentphoenix <david.vanscott@gmail.com>
2011-02-16 13:24:10 -0500

    all kinds of updates to the location class along with documentation improvements

--- nova/modules/nova/core/classes/nova/location.php
+++ nova/modules/nova/core/classes/nova/location.php
@@ -1,25 +1,28 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 /**
- * The Location class provides methods for searching through Nova's file structure to find the correct
- * view or image to pull. This is the heart and soul of seamless substitution.
+ * The Location class provides methods for searching through Nova's file structure
+ * to find the correct view or image to pull. This is the heart and soul of seamless
+ * substitution.
  *
  * @package		Nova
  * @category	Classes
  * @author		Anodyne Productions
  * @copyright	2010-11 Anodyne Productions
- * @since		2.0
+ * @since		3.0
  */
 
 abstract class Nova_Location {
 	
 	/**
-	 * Searches to find where to pull the file from. If the file exists in the skin, it'll use that,
-	 * otherwise, it'll use what's found in the common directory.
+	 * Searches to find where to pull the file from. If the file exists in the
+	 * skin, it'll use that, otherwise, it'll use what's found in the common
+	 * directory.
 	 *
 	 *     echo Location::file('main', 'default', 'templates');
 	 *     echo Location::file('main', 'default', 'layouts');
 	 *     echo Location::file('footer', 'default', 'partials');
 	 *
+	 * @access	public
 	 * @param	string	the name of the layout
 	 * @param	string	the name of the current skin
 	 * @return	string	path to the layout file
@@ -35,12 +38,16 @@ abstract class Nova_Location {
 	}
 	
 	/**
-	 * Looks for the location of the image file throughout the system. The first place this will look is in the
-	 * current skin. If the image isn't found there, it moves through the modules. Finally, if it can't be
-	 * found in the modules, it checks the nova module where it should find the image in question.
+	 * Looks for the location of an image in the system. The first place this
+	 * will look is in the current skin then in the override extension. After
+	 * that, it'll make its way through the other extensions and modules before
+	 * finally checking in the core nova module where it should find the image
+	 * in question.
 	 *
 	 *     echo Location::image('feed.png', 'default', 'main', 'image');
 	 *
+	 * @access	public
+	 * @uses	Kohana::config
 	 * @param	string	the image to find
 	 * @param	string	the skin to look in
 	 * @param	string	the section to look in
@@ -49,35 +56,12 @@ abstract class Nova_Location {
 	 */
 	public static function image($image, $skin, $section, $type = 'image')
 	{
+		$locations = self::_setup_modules(array('install', 'update', 'upgrade'))
+		
 		switch ($type)
 		{
 			case 'image':
-				// exclude these modules
-				$exclude = array('install', 'update', 'upgrade');
-				
-				// start setting up where we're gonna search
-				$locations = array(
-					APPFOLDER => APPPATH,
-				);
-				
-				// flip the modules array, which we need to do to make sure things are checked in the right order
-				$modules = array_reverse(Kohana::modules());
-				
-				// go through the modules and figure out what should and shouldn't be included
-				foreach ($modules as $m)
-				{
-					$mod = str_replace(MODPATH, '', $m);
-					
-					if ( ! in_array($mod, $exclude))
-					{
-						$loc = MODFOLDER.'/'.$mod;
-						$loc = (substr($loc, -1, 1) == "\\") ? substr_replace($loc, '', -1, 1) : $loc;
-						$loc = str_replace('\\', '/', $loc); // fixes an issue on windows machines
-						$locations[$loc] = $m;
-					}
-				}
-				
-				// set the up the path array
+				// set up the path array
 				$path = array(
 					'head' => '',
 					'skin' => '',
@@ -86,12 +70,11 @@ abstract class Nova_Location {
 					'image' => $image
 				);
 				
-				// go through the locations and try to find the image
 				foreach ($locations as $key => $l)
 				{
 					switch ($key)
 					{
-						case 'application':
+						case APPFOLDER:
 							if (is_file($l.'views/'.$skin.'/'.$section.'/images/'.$image))
 							{
 								$path['head'] = APPFOLDER.'/views';
@@ -116,36 +99,12 @@ abstract class Nova_Location {
 			break;
 				
 			case 'asset':
-				// exclude these modules
-				$exclude = array('install', 'update', 'upgrade');
-				
-				// start setting up where we're gonna search
-				$locations = array(
-					APPFOLDER => APPPATH,
-				);
-				
-				// flip the modules array, which we need to do to make sure things are checked in the right order
-				$modules = array_reverse(Kohana::modules());
-				
-				// go through the modules and figure out what should and shouldn't be included
-				foreach ($modules as $m)
-				{
-					$mod = str_replace(MODPATH, '', $m);
-					
-					if ( ! in_array($mod, $exclude))
-					{
-						$loc = MODFOLDER.'/'.$mod;
-						$locations[$loc] = $m;
-					}
-				}
-				
 				// set the up the path array
 				$path = array(
 					'head' => '',
 					'image' => $image
 				);
 				
-				// go through the locations and try to find the image
 				foreach ($locations as $key => $l)
 				{
 					if (is_file($l.'/assets/images/'.$image))
@@ -165,50 +124,31 @@ abstract class Nova_Location {
 				return APPFOLDER.'/assets/common/'.Kohana::config('nova.genre').'/ranks/'.$section.'/'.$image;
 			break;
 		}
+		
+		return false;
 	}
 	
 	/**
-	 * Looks for the location of the view file throughout the system. The first place this will look is in the
-	 * current skin. If the view file isn't found there, it moves through the modules. Finally, if it can't be
-	 * found in the modules, it checks the nova module where it should find the file in question.
+	 * Looks for the location of a view file in the system. The first place this
+	 * will look is in the current skin then in the override extension. After
+	 * that, it'll make its way through the other extensions and modules before
+	 * finally checking in the core nova module where it should find the file
+	 * in question.
 	 *
 	 *     echo Location::view('main_index', 'default', 'main', 'pages');
 	 *     echo Location::view('main_index_js', 'default', 'main', 'js');
 	 *
+	 * @access	public
 	 * @param	string	the view file
 	 * @param	string	the skin to search in
 	 * @param	string	the section to search in
 	 * @param	string	the type of view file (pages, js, ajax)
 	 * @param	string	the extension of the view file (default: .php)
-	 * @return	string	path to the view file relative to index.php
+	 * @return	string	path to the view file relative to the views directory
 	 */
 	public static function view($view, $skin, $section, $type = 'pages', $ext = EXT)
 	{
-		// exclude these modules
-		$exclude = array();
-		
-		// start setting up where we're gonna search
-		$locations = array(
-			APPFOLDER => APPPATH,
-		);
-		
-		// get all the modules
-		$modules = Kohana::modules();
-		
-		foreach ($modules as $m)
-		{
-			// figure out if it's a module or an extension
-			$modtype = (strpos($m, MODPATH) !== false) ? 'module' : 'extension';
-			
-			// make sure we're replacing the right thing based on what type of "thing" it is
-			$mod = ($modtype == 'module') ? str_replace(MODPATH, '', $m) : str_replace(EXTPATH, '', $m);
-			
-			if ( ! in_array($mod, $exclude))
-			{
-				$loc = ($modtype == 'module') ? MODFOLDER.'/'.$mod : EXTFOLDER.'/'.$mod;
-				$locations[$loc] = $m;
-			}
-		}
+		$locations = self::_setup_modules();
 		
 		// set the up the path array
 		$path = array(
@@ -245,5 +185,39 @@ abstract class Nova_Location {
 		}
 		
 		return false;
+	}
+	
+	/**
+	 * Setup the array of modules to be used by seamless substitution.
+	 *
+	 * @access	protected
+	 * @uses	Kohana::modules
+	 * @param	array 	an array of items to exclude
+	 * @return	array 	an array of modules
+	 */
+	protected static function _setup_modules($exclude = array())
+	{
+		// start setting up where we're gonna search
+		$locations = array(APPFOLDER => APPPATH);
+		
+		// get all the modules
+		$modules = Kohana::modules();
+		
+		foreach ($modules as $m)
+		{
+			// figure out if it's a module or an extension
+			$modtype = (strpos($m, MODPATH) !== false) ? 'module' : 'extension';
+			
+			// make sure we're replacing the right thing based on what type of "thing" it is
+			$mod = ($modtype == 'module') ? str_replace(MODPATH, '', $m) : str_replace(EXTPATH, '', $m);
+			
+			if ( ! in_array($mod, $exclude))
+			{
+				$loc = ($modtype == 'module') ? MODFOLDER.'/'.$mod : EXTFOLDER.'/'.$mod;
+				$locations[$loc] = $m;
+			}
+		}
+		
+		return $locations;
 	}
 }
