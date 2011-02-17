d7cb568e036c0bc0f613f33d322c03f855538dbd
agentphoenix <david.vanscott@gmail.com>
2011-02-17 12:00:49 -0500

    moved the 404 into the components directory

---
+++ nova/modules/nova/core/views/components/error/404.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title><?php echo $title;?></title>
+	</head>
+	<body>
+		<h1><?php echo $title;?></h1>
+		
+		<p><?php echo $message;?></p>
+
+		<p><a href="javascript:history.go(-1)">Go back</a></p>
+	</body>
+</html>
