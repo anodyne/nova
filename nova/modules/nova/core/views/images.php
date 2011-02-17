21be859b5d3b4cae98818581dc9d9ec70d4fc382
agentphoenix <david.vanscott@gmail.com>
2011-02-17 12:00:37 -0500

    renamed image_index to images

---
+++ nova/modules/nova/core/views/images.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Base Image Index
+ *
+ * @package		Nova
+ * @author		Anodyne Productions
+ */
+
+return array(
+	'main.error'		=> 'exclamation-red.png',
+	'main.next'			=> 'next.png',
+	'main.previous'		=> 'previous.png',
+	'main.rss'			=> 'feed.png',
+	
+	'admin.add'			=> 'add.png',
+	'admin.approve'		=> 'approve.png',
+	'admin.delete'		=> 'delete.png',
+	'admin.edit'		=> 'edit.png',
+	'admin.help'		=> 'help.png',
+	'admin.info'		=> 'info.png',
+	'admin.link'		=> 'link.png',
+	'admin.reject'		=> 'reject.png',
+);
