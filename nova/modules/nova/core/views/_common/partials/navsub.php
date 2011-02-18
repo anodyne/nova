aaad5e06234f7190226cc1e5df7bdd3fbdfe3452
agentphoenix <david.vanscott@gmail.com>
2011-02-18 07:58:06 -0500

    cleaned up the core views directory

--- nova/modules/nova/core/views/_common/partials/navsub.php
+++
@@ -1,7 +0,0 @@
-<div class="widget-1"><?php echo $widget1;?></div>
-
-<div><?php echo $menu;?></div>
-
-<div class="widget-2"><?php echo $widget2;?></div>
-
-<div class="widget-3"><?php echo $widget3;?></div>
--- nova/modules/nova/core/views/components/templates/main.php
+++ nova/modules/nova/core/views/components/templates/main.php
@@ -7,7 +7,7 @@
 <header>
 	<div class="wrapper">
 		<div class="nav-main">
-			<?php echo html::image('application/views/'.$skin.'/'.$sec.'/images/menu-nova.png', array('class' => 'float-right'));?>
+			<img src="<?php echo url::base().MODFOLDER;?>/nova/core/views/design/images/menu-nova.png" class="float-right">
 			<?php echo $navmain;?>
 		</div>
 	</div>
