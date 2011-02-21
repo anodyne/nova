4da5761f6a96934b934e75e83ab1dfccba3210ba
agentphoenix <david.vanscott@gmail.com>
2011-02-20 20:16:07 -0500

    moved the novasys configi into nova since it'll just combine the two nova.php config files into one (yay kohana)

--- nova/modules/nova/core/config/nova.php
+++ nova/modules/nova/core/config/nova.php
@@ -1,44 +1,25 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-return array(
-	/**
-	 * Version Information
-	 *
-	 * This version information is used by the various pieces of the system
-	 * to check for newer versions. Do not modify this information unless
-	 * you know what you're doing, otherwise parts of the system will stop
-	 * working.
-	 */
-	 
+return array
+(
 	'app_name' => 'Nova',
 	'app_version_full' => '3.0m3',
 	'app_version_major' => 3,
 	'app_version_minor' => 0,
 	'app_version_update' => 0,
 	
-	# TODO: remove this for final release
+	# TODO: remove before final release
 	'app_dev_build' => '20101205',
 	
-	/**
-	 * Thresher Information
-	 */
-	 
 	'wiki_name' => 'Thresher',
 	'wiki_version_full' => 'Release 3',
 	'wiki_version_major' => 2,
 	'wiki_version_minor' => 0,
 	'wiki_version_update' => 0,
 	
-	/**
-	 * Version Information Feed
-	 */
-	 
 	'version_info' => MODPATH.'nova/update/assets/version.yaml',
 	//'version_info' => 'http://www.anodyne-productions.com/feeds/version_nova.yaml',
 	
-	/**
-	 * System Information
-	 */
 	'app_db_tables' => 58,
 	'environment' => Kohana::DEVELOPMENT,
 );
