1e882df9350d47e912c0bf2dda58ceaa5f2ac819
agentphoenix <david.vanscott@gmail.com>
2011-02-20 20:17:46 -0500

    adding an empty bootstrap file to the application directory. since we moved the actual bootstrap to the nova directory, we need this here so that the server doesn't fail the installation

---
+++ application/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * We need a blank bootstrap file here otherwise the
+ * install script freaks out and says that Kohana
+ * can't be used because of missing files in the
+ * application directory. Boo Kohana!
+ */
