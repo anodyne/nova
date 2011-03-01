0b4eaf2463c1bbd549ad26a436a71074cc26de2b
agentphoenix <david.vanscott@gmail.com>
2011-03-01 09:14:46 -0500

    added the upgrade views for steps 2 and 3

---
+++ nova/modules/core/views/_base/update/pages/upgrade_step3.php
@@ -0,0 +1,42 @@
+<p class="fontMedium"><?php echo $message;?></p>
+
+<hr />
+
+<?php echo form_open('main/index');?>
+	<h3>New Password</h3>
+	
+	<p class="fontMedium"><?php echo $password;?></p>
+	
+	<p>
+		<?php echo form_input('password', 'default');?> &nbsp;
+		
+		<span class="hidden loading-password"><?php echo img(MODFOLDER.'/nova/upgrade/views/upgrade/images/loading-circle-large.gif');?></span>
+		
+		<span class="hidden error-password">
+			<?php echo img(MODFOLDER.'/nova/upgrade/views/upgrade/images/exclamation-red.png', array('class' => 'tiptip', 'title' => ''));?>
+		</span>
+		
+		<span class="hidden success-password">
+			<?php echo img(MODFOLDER.'/nova/upgrade/views/upgrade/images/tick-circle.png');?>
+		</span>
+	</p>
+	
+	<hr />
+	
+	<h3>Admin Rights</h3>
+	
+	<p class="fontMedium"><?php echo $admin;?></p>
+	
+	<p>
+		<?php echo form_dropdown('admins', $options, null, 'multiple="multiple" size="10"');?> &nbsp;
+		
+		<span class="hidden loading-admins"><?php echo img(MODFOLDER.'/nova/upgrade/views/upgrade/images/loading-circle-large.gif');?></span>
+		
+		<span class="hidden error-admins">
+			<?php echo img(MODFOLDER.'/nova/upgrade/views/upgrade/images/exclamation-red.png', array('class' => 'tiptip', 'title' => ''));?>
+		</span>
+		
+		<span class="hidden success-admins">
+			<?php echo img(MODFOLDER.'/nova/upgrade/views/upgrade/images/tick-circle.png');?>
+		</span>
+	</p>
