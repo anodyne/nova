63288ca64e55be9a392163a3f8c88c82374b2615
agentphoenix <david.vanscott@gmail.com>
2011-03-27 18:21:20 -0400

    fixed error thrown with the user panel library in some cases

--- application/libraries/User_panel.php
+++ application/libraries/User_panel.php
@@ -1,5 +1,5 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-require_once MODPATH.'core/libraries/nova_user_panel'.EXT;
+require_once MODPATH.'core/libraries/Nova_user_panel'.EXT;
 
 class User_panel extends Nova_user_panel {}
