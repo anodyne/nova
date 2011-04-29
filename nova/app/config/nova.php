25c416de99334093c686345dd066eb68402dd973
agentphoenix <david.vanscott@gmail.com>
2011-04-29 13:32:54 -0400

    added some items to the nova config file

--- nova/app/config/nova.php
+++ nova/app/config/nova.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-return array
-(
+return array(
+	
 	'app_name' => 'Nova',
 	'app_version_full' => '3.0m3',
 	'app_version_major' => 3,
@@ -16,6 +16,12 @@ return array
 	'wiki_version_major' => 2,
 	'wiki_version_minor' => 0,
 	'wiki_version_update' => 0,
+	
+	'forum_name' => 'Mako',
+	'forum_version_full' => 'Release 1',
+	'forum_version_major' => 0,
+	'forum_version_minor' => 0,
+	'forum_version_update' => 0,
 	
 	'version_info' => MODPATH.'app/modules/setup/assets/update/version.yaml',
 	//'version_info' => 'http://www.anodyne-productions.com/feeds/version_nova.yaml',
