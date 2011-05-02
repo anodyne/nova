202e98add6f7c80e9282a3a177c024f48025bfe4
agentphoenix <david.vanscott@gmail.com>
2011-05-02 10:57:25 -0400

    [fusion] Got rid of some inefficiencies with variable static calls during hydration.

--- nova/modules/fusion/classes/fusion/query.php
+++ nova/modules/fusion/classes/fusion/query.php
@@ -674,10 +674,6 @@ class Fusion_Query {
 	 */
 	public function hydrate(&$row, $models, &$result, $model = null, $select = null, $primary_key = null)
 	{
-		$model = is_null($model) ? $this->model : $model;
-		$select = is_null($select) ? $this->select() : $select;
-		$primary_key = is_null($primary_key) ? $model::primary_key() : $primary_key;
-
 		// First check the PKs, if null it's an empty row
 		$r1c1    = reset($select);
 		$prefix  = substr($r1c1[0], 0, strpos($r1c1[0], '.') + 1);
@@ -698,7 +694,7 @@ class Fusion_Query {
 		}
 
 		// Check for cached object
-		$pk  = $model::implode_pk($obj);
+		$pk  = count($primary_key) == 1 ? reset($obj) : '['.implode('][', $obj).']';
 		$obj = Model::cached_object($pk, $model);
 
 		// Create the object when it wasn't found
@@ -812,9 +808,12 @@ class Fusion_Query {
 
 		$rows = $query->execute()->as_array();
 		$result = array();
+		$model = $this->model;
+		$select = $this->select();
+		$primary_key = $model::primary_key();
 		foreach ($rows as $id => $row)
 		{
-			$this->hydrate($row, $models, $result);
+			$this->hydrate($row, $models, $result, $model, $select, $primary_key);
 			unset($rows[$id]);
 		}
 
