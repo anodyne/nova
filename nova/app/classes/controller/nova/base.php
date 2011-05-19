e6506c3a7302f0dd11d117eea0d85f1ca28aba21
agentphoenix <david.vanscott@gmail.com>
2011-05-19 16:22:48 -0400

    updated the docblocks in the base controller, added the after method, added the controller action data property and made sure things use the _data property

--- nova/app/classes/controller/nova/base.php
+++ nova/app/classes/controller/nova/base.php
@@ -1,48 +1,86 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php
 /**
  * Base Controller
+ *
+ * The base controller is used by every single controller in the system and
+ * finalizes setting the system up prior to controller action execution. In
+ * addition, the base controller ensures that some classes and variables are
+ * always availabl in the controllers.
  *
  * @package		Nova
  * @category	Controllers
  * @author		Anodyne Productions
  * @copyright	2011 Anodyne Productions
- * @version		3.0
  */
 
 abstract class Controller_Nova_Base extends Controller_Template {
 	
-	// these libraries should be globally available
+	/**
+	 * @var	Session	The current session instance.
+	 */
 	public $session;
 	
-	// the options object
+	/**
+	 * @var	object	An object that stores all of the setting values from the database.
+	 */
 	public $options;
 	
-	// the images array
+	/**
+	 * @var	array	All the image information from the image indices.
+	 */
 	public $images;
 	
-	// the important system settings
+	/**
+	 * @var	string	The current skin.
+	 */
 	public $skin;
+	
+	/**
+	 * @var	string	The current rank set.
+	 */
 	public $rank;
+	
+	/**
+	 * @var	string	The current timezone.
+	 */
 	public $timezone;
+	
+	/**
+	 * @var	bool	Whether we're in daylight savings time.
+	 */
 	public $dst;
 	
-	// the array of setting keys to pull
+	/**
+	 * @var	array	An array of setting keys used to make the final pull from the database.
+	 */
 	protected $settingsArray;
 	
+	/**
+	 * @var	object	Controller action data.
+	 */
+	protected $_data;
+	
+	/**
+	 * The before method handles setting up the controller before any action
+	 * methods are called.
+	 *
+	 * @access	public
+	 * @return	void
+	 */
 	public function before()
 	{
 		parent::before();
 		
 		// if the config file isn't set
-		if ( ! file_exists(APPPATH.'config/database'.EXT))
+		if ( ! file_exists(APPPATH.'config/database.php'))
 		{
-			$this->request->redirect('install/setupconfig');
+			$this->request->redirect('setup/main/config');
 		}
 		
 		// make sure the system is installed
 		if ( ! Utility::install_status())
 		{
-			$this->request->redirect('install/index');
+			$this->request->redirect('setup/main/index');
 		}
 		
 		// load the session library
@@ -63,5 +101,30 @@ abstract class Controller_Nova_Base extends Controller_Template {
 			'sim_name',
 			'system_email'
 		);
+		
+		// make the data variable an object
+		$this->_data = new stdClass;
+	}
+	
+	/**
+	 * The after method handles the final steps before rendering the output to
+	 * the browser, including populating the template variable with all of the
+	 * information it needs from the controller action.
+	 *
+	 * @access	public
+	 * @return	void
+	 */
+	public function after()
+	{
+		parent::after();
+		
+		// set the content 
+		$this->template->layout->content = $this->_data;
+		
+		// set the final template data
+		$this->template->title.= $this->_data->title;
+		
+		// send the response
+		$this->response->body($this->template);
 	}
 }
