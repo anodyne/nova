5d36b48d12bd54eb718c555de2e0fa200b426da6
agentphoenix <david.vanscott@gmail.com>
2011-05-31 11:25:24 -0400

    all kinds of models will have a create_SOMETHING method, so instead of duplicating all kinds of code, let's put it in fusion and then we'll have access to it for every model

--- nova/modules/fusion/classes/model.php
+++ nova/modules/fusion/classes/model.php
@@ -1,3 +1,32 @@
 <?php
 
-class Model extends Fusion_Model {}
+class Model extends Fusion_Model {
+	
+	/**
+	 * Create a an item.
+	 *
+	 * @access	public
+	 * @param	mixed	an array or object of data
+	 * @return	object	the newly created item
+	 */
+	public static function create_item($data)
+	{
+		$item = static::factory();
+		
+		foreach ($data as $key => $value)
+		{
+			if (is_array($data))
+			{
+				$item->{$key} = $data[$key];
+			}
+			else
+			{
+				$item->{$key} = $data->{$key};
+			}
+		}
+		
+		$item->save();
+		
+		return $item;
+	}
+}
