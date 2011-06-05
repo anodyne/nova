db9ddab5848669f0a3cf3ef226df7ccf24808a2a
agentphoenix <david.vanscott@gmail.com>
2011-06-05 13:42:11 -0400

    working on getting the contentEditable stuff up and running

--- nova/app/views/components/structure/main.php
+++ nova/app/views/components/structure/main.php
@@ -10,32 +10,39 @@
 		
 		<?php if (isset($_redirect)): echo $_redirect; endif;?>
 		
-		<?php if (is_file(APPPATH.'views/'.$skin.'/design/style.css')): ?>
-			<?php echo html::style(APPFOLDER.'/views/'.$skin.'/design/style.css');?>
-		<?php else: ?>
-			<?php echo html::style(MODFOLDER.'/app/views/design/style.css');?>
-			
-			<?php if (is_file(APPPATH.'views/'.$skin.'/design/custom.css')): ?>
-				<?php echo html::style(APPFOLDER.'/views/'.$skin.'/design/custom.css');?>
-			<?php endif;?>
-		<?php endif;?>
-		
 		<!-- jQUERY UI TABS STYLESHEET -->
 		<?php if (is_file(APPPATH.'views/'.$skin.'/'.$sec.'/css/jquery.ui.tabs.css')): ?>
 			<?php //echo html::style(APPFOLDER.'/views/'.$skin.'/'.$sec.'/css/jquery.ui.tabs.css');?>
 		<?php else: ?>
-			<?php echo html::style(MODFOLDER.'/modules/assets/css/jquery.ui.tabs.css');?>
+			<?php echo Html::style(MODFOLDER.'/modules/assets/css/jquery.ui.tabs.css');?>
 		<?php endif;?>
 		
 		<!--[if lt IE 9]>
 		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
 		
-		<?php echo html::script(MODFOLDER.'/modules/assets/js/jquery.js');?>
-		<?php echo html::script(MODFOLDER.'/modules/assets/js/jquery.ui.core.min.js');?>
-		<?php echo html::script(MODFOLDER.'/modules/assets/js/jquery.ui.widget.min.js');?>
-		<?php echo html::script(MODFOLDER.'/modules/assets/js/jquery.ui.tabs.min.js');?>
+		<?php echo Html::script(MODFOLDER.'/modules/assets/js/jquery.js');?>
+		<?php echo Html::script(MODFOLDER.'/modules/assets/js/jquery.ui.core.min.js');?>
+		<?php echo Html::script(MODFOLDER.'/modules/assets/js/jquery.ui.widget.min.js');?>
+		<?php echo Html::script(MODFOLDER.'/modules/assets/js/jquery.ui.tabs.min.js');?>
+		
+		<?php echo Html::script(MODFOLDER.'/modules/assets/contenteditable/shortcut.js');?>
+		<?php echo Html::script(MODFOLDER.'/modules/assets/contenteditable/farbtastic/farbtastic.js');?>
+		<?php echo Html::script(MODFOLDER.'/modules/assets/contenteditable/freshereditor.js');?>
+		<?php echo Html::style(MODFOLDER.'/modules/assets/contenteditable/freshereditor.css');?>
+		<?php echo Html::style(MODFOLDER.'/modules/assets/contenteditable/farbtastic/farbtastic.css');?>
+		
 		<?php echo $javascript;?>
+		
+		<?php if (is_file(APPPATH.'views/'.$skin.'/design/style.css')): ?>
+			<?php echo Html::style(APPFOLDER.'/views/'.$skin.'/design/style.css');?>
+		<?php else: ?>
+			<?php echo Html::style(MODFOLDER.'/app/views/design/style.css');?>
+			
+			<?php if (is_file(APPPATH.'views/'.$skin.'/design/custom.css')): ?>
+				<?php echo Html::style(APPFOLDER.'/views/'.$skin.'/design/custom.css');?>
+			<?php endif;?>
+		<?php endif;?>
 	</head>
 	<body>
 		<?php echo $layout;?>
