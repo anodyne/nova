0d3ab10cd3daf3ab1bc479c8bf1166e93eaf6574
agentphoenix <david.vanscott@gmail.com>
2011-06-15 15:16:46 -0400

    renamed the 2.0 update file to take in to account that there will be another nova 1 release before 2.0

--- nova/modules/assets/update/update_125.php
+++ nova/modules/assets/update/update_125.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /**
- * Update Nova from 1.2.4 to 2.0
+ * Update Nova from 1.2.5 to 2.0
  */
 $system_versions	= null;
 $system_info		= null;
@@ -21,20 +21,32 @@ $system_versions = array(
 	'version_update'	=> 0,
 	'version_date'		=> 1308949200,
 	'version_launch'	=> "You've spoken and we've listened. The feedback we constantly get about Nova is that it's great, but it's difficult to update. Nova 2 is all about fixing that very issue. With a brand new file structure, Nova 2 has never been easier to update (simply delete one folder and replace it with one from the zip archive). In addition, Nova 2 adds new functionality to the system to help admins manage their RPG. Nova 2 is smarter than before, tracking who did and who didn't participate in a post. If someone didn't add anything to the post, they'll automatically be removed before it's posted (this feature can be turned on and off from Site Settings). In addition, Thresher has gotten a much needed boost from R1 to R2 which adds new page management and page viewing interfaces and a new category selection process that allows admins to add categories on the fly. More information about these features and everything else in Nova 2 (plus a full changelog) can be found at AnodyneDocs. This update is recommended for all users.",
-	'version_changes'	=> "* added the message.php file to handle notification of bans, a missing \"nova\" directory and incompatible PHP version
+	'version_changes'	=> "* brand new file structure that further separates the nova core from user modifications and makes updating infinitely easier
+* added the message.php file to handle notification of bans, a missing 'nova' directory and incompatible PHP version
 * added new process to write the database config file for you
 * added the ability to upgrade SMS Database entries to Thresher wiki pages
-* added the ability for textareas to \"grow\" as more text is added like Facebook
+* added the ability for textareas to 'grow' as more text is added like Facebook
+* added the ability for site messages to have previously disallowed HTML tags (like embed, iframe, etc.) for embedding media assets from YouTube and Vimeo
+* added the ability to nest mission groups one level deep
 * updated seamless substitution to be able to override email view files
 * updated Thresher with a new way to create and manage categories when working on a wiki page
 * updated Thresher with a completely new user experience for managing wiki pages
 * updated Thresher with a brand new interface for viewing wiki pages
 * updated the upload instructions to include the maximum file size and maximum image dimensions from the config file for reference
+* updated the deck listing page (sim/decks) to not use a table which makes for a much cleaner layout
+* updated the deck listing page (sim/decks) to have a menu of decks for quickly moving to a deck item without having to scroll (handy for sim with lots of decks)
 * updated to jquery version 1.6.1
 * updated to jquery version 1.8.13
 * updated to uniform version 1.7.5
 * updated to prettyPhoto version 3.1.2
 * updated to qTip2
+* updated the database to not use a default value for a character's rank to avoid confusion when dealing with pending characters
+* updated the UI for listing mission groups to provide more information and look a lot better
+* updated the missions model to allow group missions to be pulled from the get_all_missions method
+* updated the missions model with a method to count mission groups
+* updated the users model with a method to pull all of a users' LOA records
+* updated the mission post writing page to show who owns a linked NPC
+* updated the skin catalogue to allow removing an entire skin (with sections) and letting admins choose which skin users will be updated to for each section
 * refactored the location helper into a full-blown class with static methods
 * refactored the upgrade process to mirror what was created for nova 3
 * removed the banned.php file
@@ -42,7 +54,9 @@ $system_versions = array(
 * fixed bug with seamless substitution of images where they wouldn't work when they were in the _base_override directory
 * fixed bug with private messages where RE: and FWD: would constantly be added to message, now Nova will make sure it's only added once
 * fixed bug with private messages where the person sending the message would be on the recipient list, so any message they sent would show up in their inbox as well
-* fixed bug where the join form could be submitted without an email address or password"
+* fixed bug where the join form could be submitted without an email address or password
+* fixed bug where users who were deactivated kept their account flags (sysadmin, game master, etc.) and their access role
+* fixed bug where users who were reactivated didn't have their access role set to Standard User"
 );
 
 $system_info = array(
