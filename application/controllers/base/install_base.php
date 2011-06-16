bdf6e6d547caed22f0fc18e4db463c554af35070
agentphoenix <david.vanscott@gmail.com>
2011-06-16 10:43:42 -0400

    changed the registration code to point to a new sub-domain on the AP site

--- application/controllers/base/install_base.php
+++ application/controllers/base/install_base.php
@@ -1498,7 +1498,7 @@ class Install_base extends Controller {
 		$this->load->library('email');
 		
 		/* set up the server and method for the request */
-		$this->xmlrpc->server('http://www.anodyne-productions.com/index.php/utility/do_registration', 80);
+		$this->xmlrpc->server('http://register.anodyne-productions.com/index.php/utility/do_registration', 80);
 		$this->xmlrpc->method('Do_Registration');
 		
 		/* build the request */
--- application/controllers/base/update_base.php
+++ application/controllers/base/update_base.php
@@ -654,7 +654,7 @@ class Update_base extends Controller {
 		$this->load->library('email');
 		
 		/* set up the server and method for the request */
-		$this->xmlrpc->server('http://www.anodyne-productions.com/index.php/utility/do_registration', 80);
+		$this->xmlrpc->server('http://register.anodyne-productions.com/index.php/utility/do_registration', 80);
 		$this->xmlrpc->method('Do_Registration');
 		
 		/* build the request */
