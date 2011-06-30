b0b36095c5893a2af256752b5d59845dba63f50b
agentphoenix <david.vanscott@gmail.com>
2011-06-30 14:07:12 -0400

    removed the API from nova 2.0 (will re-add in a later version of nova 2)

--- application/controllers/api.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once MODPATH.'core/controllers/nova_api'.EXT;
-
-class Api extends Nova_api {}
--- application/libraries/Format.php
+++
@@ -1,5 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-require_once MODPATH.'core/libraries/Nova_format'.EXT;
-
-class Format extends Nova_format {}
