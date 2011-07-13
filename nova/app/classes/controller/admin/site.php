5248b0c09712f238abead1ca298c0997f5078499
agentphoenix <david.vanscott@gmail.com>
2011-07-13 08:24:08 -0400

    removing the site controllers since we're not going to be using them

--- nova/app/classes/controller/admin/site.php
+++
@@ -1,9 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Controller_Admin_Site extends Controller_Nova_Admin_Site {
-	
-	public function before()
-	{
-		parent::before();
-	}
-}
