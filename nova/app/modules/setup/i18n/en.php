6301f4294d434293ed3bda48c4f2edd304c39a7e
David VanScott <david.vanscott@gmail.com>
2011-08-23 15:43:49 -0400

    language updates

--- nova/app/modules/setup/i18n/en.php
+++ nova/app/modules/setup/i18n/en.php
@@ -79,7 +79,7 @@ return array(
 				'errors' => "Uh oh! I couldn't go to the next step because there are some errors with what you filled in. Check the messages below, make any corrections and give it another try.",
 			),
 			'step2' => array(
-				'instructions' => "I bet you were expecting more steps, huh? Sorry to disappoint you, but Nova is installed and ready to use. Head on over to your site now to check it out and start using your Nova site.",
+				'instructions' => "Expecting more steps? Sorry to disappoint you, but Nova is installed and ready to use. Head on over to your site now to get started.",
 			),
 		),
 		
@@ -95,8 +95,18 @@ return array(
 			),
 		),
 		
-		'genre' => array(
-			'message' => "Welcome to the Genre Panel. From here, you can see the status of genres and either install or uninstall genres as needed. Make sure you use great caution when removing genres as it can cause the entire system to break. The only limitation you have is that you cannot uninstall the current genre. If you want to uninstall the current genre, you'll need to change your Nova config file (<code>:path</code>), save and upload the file, then come back here to uninstall that genre.",
+		'remove' => array(
+			'instructions' => "Whoa, hold up! Uninstalling Nova will remove all the data in the database tables (posts, logs, characters, etc.) and cannot be undone. Make absolutely sure you want to do this before continuing.",
+			'success' => "Poof! I was able to successfully uninstall Nova 3. Now, you can go back to the Setup Center to re-install Nova 3 or upgrade from Nova 2.",
+			'failure' => "Uh oh! I wasn't able to uninstall Nova 3. Try again or if the problem continues, you can manually remove the database tables from the database.",
+			'no_tables' => "I couldn't find a Nova 3 installation to remove.",
+		),
+		
+		'change' => array(
+			'default' => "You can use the sections below to modify your database for any changes you'd like to make. You can only add tables and fields, you cannot delete or modify existing tables or fields. In addition, you can only take these actions on Nova's tables, no other tables in your database. <span class='bold error'>Use extreme caution when modifying the database!</span>",
+			'query' => "I can run any properly formatted MySQL query you have (like something that may have come with a MOD). Simply paste the query into the field below and I'll execute the query. <strong class='error'>MySQL queries can cause a lot of damage to your Nova database so make sure you trust whoever gave you the query and understand what the query is trying to do!</strong>",
+			'field' => "Need a new field in one of the Nova database tables? I can create a new field for you, all you need to do is tell me a little about the field you want to create.",
+			'table' => "Need a new database table for a MOD or something cool you're working on? I can create a new database table for you, all you need to do is tell me what you want to call the table. Don't worry about adding the table prefix, I'll do that before creating the table as well as an ID field for you. If you want to change the ID field, you'll have to do that from inside the database.",
 		),
 	),
 );
