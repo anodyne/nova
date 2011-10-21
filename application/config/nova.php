2e4ed3fad57baec15de37255c89cd906b1514ff8
David VanScott <david.vanscott@gmail.com>
2011-10-20 21:25:06 -0400

    Remove the meta data from the config file
    Meta data is now managed through Site Settings

--- application/config/nova.php
+++ application/config/nova.php
@@ -15,18 +15,6 @@ return array(
 	'genre' => 'ds9',
 	
 	/**
-	 * Meta Data
-	 *
-	 * Meta data is, essentially, data about data. Use these meta tags
-	 * (if you want) to specify the meta data found in the head of the
-	 * layouts of the system. More information can be found about meta
-	 * tags by doing a simple Google search from your web browser.
-	 */
-	'meta_desc'		=> "Anodyne Productions' premier online RPG management software",
-	'meta_author'	=> "Anodyne Productions",
-	'meta_keywords'	=> "nova, rpg management, anodyne, rpg, sms",
-	
-	/**
 	 * RSS Feed Settings
 	 *
 	 * Change these values if you want tochange the way your RSS feeds
