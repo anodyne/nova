ddcb22f3c9899fe647c9195520b94ead6907c62a
David VanScott <david.vanscott@gmail.com>
2012-04-16 16:15:54 -0400

    Add the first pieces of an error page for the admin section

--- nova/modules/nova/classes/controller/admin.php
+++ nova/modules/nova/classes/controller/admin.php
@@ -11,6 +11,12 @@ namespace Nova;
 
 class Controller_Admin extends Controller_Base_Admin
 {
+	/**
+	 * Admin Error Codes
+	 */
+	const OK 				= 0;
+	const NOT_ALLOWED 		= 1;
+
 	public function before()
 	{
 		parent::before();
@@ -23,5 +29,10 @@ class Controller_Admin extends Controller_Base_Admin
 		$this->_view = 'admin/index';
 		
 		return;
+	}
+
+	public function action_error($error = 0)
+	{
+		# code...
 	}
 }
