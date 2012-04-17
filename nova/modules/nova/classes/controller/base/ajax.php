10ab4e2d21e1e58a8bf2f875c5ca82fd003779af
David VanScott <david.vanscott@gmail.com>
2012-04-17 16:14:30 -0400

    Add a base Ajax controller

---
+++ nova/modules/nova/classes/controller/base/ajax.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Nova's ajax controller.
+ *
+ * @package		Nova
+ * @category	Controller
+ * @author		Anodyne Productions
+ * @copyright	2012 Anodyne Productions
+ */
+
+namespace Nova;
+
+class Controller_Base_Ajax extends \Controller
+{
+	public function before()
+	{
+		parent::before();
+
+		// manually add the nova module to the paths
+		\Finder::instance()->add_path(\Fuel::add_module('nova'));
+		
+		// go out and load then merge the nova config files
+		\Config::load('nova', true, false, true);
+
+		// load the language files
+		\Lang::load('app');
+		\Lang::load('nova::base');
+		\Lang::load('nova::event', 'event');
+		\Lang::load('nova::email', 'email');
+		\Lang::load('nova::action', 'action');
+		\Lang::load('nova::short', 'short');
+		\Lang::load('nova::status', 'status');
+		\Lang::load('nova::sitecontent', 'sitecontent');
+	}
+	
+	public function after($response)
+	{
+		parent::after($response);
+		
+		// return the response object
+		return $this->response;
+	}
+}
