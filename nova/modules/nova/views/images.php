58bbdb8815a9a6a020865c2cd4c867d82f964d85
David VanScott <david.vanscott@gmail.com>
2012-04-20 13:37:21 -0400

    Add some new images and update the image index

--- nova/modules/nova/views/images.php
+++ nova/modules/nova/views/images.php
@@ -1,7 +1,12 @@
 <?php
 
 return array(
+	'fields' => 'ui-text-field.png',
+	
 	'reorder' => 'reorder-16x16.png',
 	
+	'sections' => 'ui-group-box.png',
 	'settings' => 'gear-16x16.png',
+
+	'tabs' => 'ui-tab.png',
 );
