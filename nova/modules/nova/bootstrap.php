04c0ea5b2faf4190edd91b36ba55c12f4b3cd1d3
David VanScott <david.vanscott@gmail.com>
2012-05-02 15:43:26 -0400

    Whitespace update

--- nova/modules/nova/bootstrap.php
+++ nova/modules/nova/bootstrap.php
@@ -7,7 +7,6 @@ class_alias('Fuel\\Core\\Autoloader', 'Autoloader');
 // Bootstrap the framework DO NOT edit this
 require COREPATH.'bootstrap.php';
 
-
 Autoloader::add_classes(array(
 	// Add classes you want to override here
 	// Example: 'View' => APPPATH.'classes/view.php',
