ad71576b13a9d2a2f7a4fe5576003ba132de0771
David VanScott <david.vanscott@gmail.com>
2012-05-02 14:52:53 -0400

    Update the number of tables in the base install
    We have to take in to account the new migration table

--- nova/modules/nova/config/nova.php
+++ nova/modules/nova/config/nova.php
@@ -24,7 +24,7 @@ return array(
 	//'version_info' => 'http://www.anodyne-productions.com/feeds/version_nova.json',
 	
 	'dev_install' => false,
-	'base_install_tables' => 58,
+	'base_install_tables' => 59,
 	
 	'environment' => \Fuel::DEVELOPMENT,
 );
