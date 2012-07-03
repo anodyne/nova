6b43e52804848f38a99be58501fa708ae8744479
David VanScott <david.vanscott@gmail.com>
2012-07-03 15:05:58 -0400

    Update some comments

--- app/lang/en/app.php
+++ app/lang/en/app.php
@@ -1,3 +1,9 @@
 <?php
+/**
+ * Specific language items can be overwritten with your own terms in here using
+ * an array. Simply find the array key and then set the value to your own.
+ *
+ *     return array('position' => 'job');
+ */
 
 return array();
