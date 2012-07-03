797e1404e3cd8c486c1583629eed2a5cb9df5b44
David VanScott <david.vanscott@gmail.com>
2012-07-03 09:59:18 -0400

    Add the dev migrations config file

---
+++ app/config/development/migrations.php
@@ -0,0 +1,88 @@
+<?php
+return array(
+	'version' => 
+	array(
+		'app' => 
+		array(
+			'default' => 0,
+		),
+		'module' => 
+		array(
+			'setup' => 
+			array(
+				0 => '001_create_roles',
+				1 => '002_create_roles_tasks',
+				2 => '003_create_tasks',
+				3 => '004_create_announcements',
+				4 => '005_create_announcement_categories',
+				5 => '006_create_applications',
+				6 => '007_create_awards',
+				7 => '008_create_awards_categories',
+				8 => '009_create_awards_queue',
+				9 => '010_create_awards_received',
+				10 => '011_create_bans',
+				11 => '012_create_catalog_modules',
+				12 => '013_create_catalog_ranks',
+				13 => '014_create_catalog_skins',
+				14 => '015_create_catalog_skinsecs',
+				15 => '016_create_catalog_widgets',
+				16 => '017_create_characters',
+				17 => '018_create_character_images',
+				18 => '019_create_character_positions',
+				19 => '020_create_character_promotions',
+				20 => '021_create_comments',
+				21 => '022_create_departments',
+				22 => '023_create_forms',
+				23 => '024_create_form_data',
+				24 => '025_create_form_fields',
+				25 => '026_create_form_sections',
+				26 => '027_create_form_tabs',
+				27 => '028_create_form_values',
+				28 => '029_create_manifests',
+				29 => '030_create_media',
+				30 => '031_create_messages',
+				31 => '032_create_message_recipients',
+				32 => '033_create_missions',
+				33 => '034_create_mission_groups',
+				34 => '035_create_moderation',
+				35 => '036_create_navigation',
+				36 => '037_create_personal_logs',
+				37 => '038_create_positions',
+				38 => '039_create_posts',
+				39 => '040_create_post_authors',
+				40 => '041_create_ranks',
+				41 => '042_create_sessions',
+				42 => '043_create_settings',
+				43 => '044_create_sim_types',
+				44 => '045_create_site_contents',
+				45 => '046_create_specs',
+				46 => '047_create_system_info',
+				47 => '048_create_system_events',
+				48 => '049_create_tour',
+				49 => '050_create_tour_decks',
+				50 => '051_create_users',
+				51 => '052_create_user_loas',
+				52 => '053_create_users_preferences',
+				53 => '054_create_users_suspended',
+				54 => '055_create_wiki_categories',
+				55 => '056_create_wiki_drafts',
+				56 => '057_create_wiki_pages',
+				57 => '058_create_wiki_restrictions',
+			),
+			'coc' => 
+			array(
+				0 => '001_create_coc',
+			),
+			'docking' => 
+			array(
+				0 => '001_create_docking',
+				1 => '002_upgrade_nova2_docking',
+			),
+		),
+		'package' => 
+		array(
+		),
+	),
+	'folder' => 'migrations/',
+	'table' => 'migration',
+);
