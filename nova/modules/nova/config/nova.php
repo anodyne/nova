fcf5a8c4b535bb8c54e2510a524829cc4a7b2ad3
David VanScott <david.vanscott@gmail.com>
2012-07-03 09:59:32 -0400

    There are 60 tables in Nova, not 59

--- nova/modules/nova/config/nova.php
+++ nova/modules/nova/config/nova.php
@@ -24,7 +24,7 @@ return array(
 	//'version_info' => 'http://www.anodyne-productions.com/feeds/version_nova.json',
 	
 	'dev_install' => false,
-	'base_install_tables' => 59,
+	'base_install_tables' => 60,
 	
 	'environment' => \Fuel::DEVELOPMENT,
 );
