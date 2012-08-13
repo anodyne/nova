bb2ce0f738b261cc0ffb2f68d0baf8ba85a4cefa
David VanScott <david.vanscott@gmail.com>
2012-08-13 16:09:54 -0400

    Update the config to NOT globally filter xss

--- nova/modules/nova/config/config.php
+++ nova/modules/nova/config/config.php
@@ -113,7 +113,7 @@ return array(
 		 * WARNING: Using xss_clean will cause a performance hit.  How much is
 		 * dependant on how much input data there is.
 		 */
-		'input_filter'  => array('xss_clean'),
+		'input_filter'  => array(),
 
 		/**
 		 * This output filter can be any normal PHP function as well as 'xss_clean'
