ef8a4f5ad40f0e205ac9eba08e21690a7b07932c
David VanScott <david.vanscott@gmail.com>
2012-08-14 12:54:24 -0400

    Update the setup ajax controller to be more efficient

--- nova/modules/setup/classes/controller/ajax.php
+++ nova/modules/setup/classes/controller/ajax.php
@@ -18,11 +18,10 @@ class Controller_Ajax extends \Controller
 		# TODO: need to make sure they have access as well
 		if (\Sentry::check())
 		{
-			// get the version
-			$version = \Security::xss_clean(\Input::post('version'));
-			
 			// update the system information table with the ignore version
-			\Model_System::update_info(array('version_ignore' => $version));
+			\Model_System::update_info(array(
+				'version_ignore' => \Security::xss_clean(\Input::post('version'))
+			));
 		}
 		
 		return;
