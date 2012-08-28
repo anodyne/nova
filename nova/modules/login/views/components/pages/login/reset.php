3ce7c0552f12834ca66de5f383baa091ad75d95d
David VanScott <david.vanscott@gmail.com>
2012-08-28 15:50:08 -0400

    Tweak the login page layouts

--- nova/modules/login/views/components/pages/login/reset.php
+++ nova/modules/login/views/components/pages/login/reset.php
@@ -2,17 +2,13 @@
 <?php echo Form::open('login/reset');?>
 	<div class="control-group">
 		<div class="controls">
-			<div class="input-prepend">
-				<span class="add-on"><div class="icn icn16 icn-50" data-icon="e"></div></span><input type="email" name="email" id="email" placeholder="<?php echo lang("email_address", 2);?>">
-			</div>
+			<input type="email" name="email" id="email" class="span4" placeholder="<?php echo lang("email_address", 2);?>">
 		</div>
 	</div>
 	
 	<div class="control-group">
 		<div class="controls">
-			<div class="input-prepend input-append">
-				<span class="add-on"><div class="icn icn16 icn-50" data-icon="("></div></span><input type="password" name="password" id="password" placeholder="<?php echo lang('status.new password', 2);?>"><span class="add-on"><a href="<?php echo Uri::create('login/index');?>" class="tooltip-right" title="<?php echo lang('short.cancel_password_reset');?>"><div class="icn icn16 icn-50" data-icon="2"></div></a></span>
-			</div>
+			<input type="password" name="password" id="password" class="span4" placeholder="<?php echo lang('status.new password', 2);?>">
 		</div>
 	</div>
 
@@ -20,6 +16,7 @@
 		<div class="controls">
 			<br>
 			<button class="btn btn-primary"><?php echo lang('action.submit', 1);?></button>
+			<a href="<?php echo Uri::create('login/index');?>" class="btn"><?php echo lang('short.cancel_password_reset');?></a>
 		</div>
 	</div>
 	<?php echo Form::hidden(Config::get('security.csrf_token_key'), Security::fetch_token());?>
