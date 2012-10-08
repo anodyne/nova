7b144022fecc1c65645b4aa46e21130f35f89ee3
David VanScott <david.vanscott@gmail.com>
2012-10-08 08:27:25 -0400

    Add nav partials

---
+++ nova/modules/nova/views/components/partials/nav/dropdown.php
@@ -0,0 +1,21 @@
+<div class="navbar navbar-fixed-top">
+	<div class="navbar-inner">
+		<div class="container">
+			<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</a>
+
+			<a href="<?php echo Uri::create('main/index');?>" class="brand"><?php echo $sim_name;?></a>
+
+			<div class="nav-collapse">
+				<ul class="nav">
+
+				</ul>
+				<?php echo $navmain;?>
+				<?php echo $navuser;?>
+			</div>
+		</div>
+	</div>
+</div>
