b2296d2e29329cc2c57ceb5435eadaa5c960d1c2
David VanScott <david.vanscott@gmail.com>
2012-10-29 16:17:53 -0400

    Update the user nav partial

---
+++ nova/modules/nova/views/components/partials/nav/user.php
@@ -0,0 +1,23 @@
+<ul class="nav pull-right">
+	<?php if ($loggedIn): ?>
+		<li class="dropdown">
+			<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+				<?php if ($notifyTotal > 0): ?><span class="label<?php echo $notifyClass;?>"><?php echo $notifyTotal;?></span><?php endif;?> <?php echo $name;?> <b class="caret"></b>
+			</a>
+
+			<ul class="dropdown-menu">
+			<?php foreach ($data as $key => $section): ?>
+				<?php if ($key != 0): ?>
+					<li class="divider"></li>
+				<?php endif;?>
+				
+				<?php foreach ($section as $item): ?>
+					<li><?php echo Html::anchor($item['url'], $item['name'].$item['additional'], $item['extra']);?></li>
+				<?php endforeach;?>
+			<?php endforeach;?>
+			</ul>
+		</li>
+	<?php else: ?>
+		<li><a href="<?php echo Uri::create('login/index');?>"><?php echo lang('action.login', 2);?></a></li>
+	<?php endif;?>
+</ul>
