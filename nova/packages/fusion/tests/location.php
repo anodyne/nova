d88dc2756dc2584f07f6b440dfb72366eb5a6307
David VanScott <david.vanscott@gmail.com>
2012-10-30 12:38:45 -0400

    Add some placeholder tests for the Location and Nav classes

---
+++ nova/packages/fusion/tests/location.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Location class tests
+ *
+ * @group	Fusion
+ */
+
+namespace Fusion;
+
+class Test_Location extends TestCase
+{
+	protected function setUp()
+	{}
+
+	public function test_foo()
+	{}
+}
---
+++ nova/packages/fusion/tests/nav.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Nav class tests
+ *
+ * @group	Fusion
+ */
+
+namespace Fusion;
+
+class Test_Nav extends TestCase
+{
+	protected function setUp()
+	{}
+
+	public function test_foo()
+	{}
+}
