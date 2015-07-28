9894f50548a82e31aa678255159b69b542edaa42
David VanScott <david.vanscott@gmail.com>
2015-07-28 07:58:54 -0400

    Fix manifest toggles for alternate HTML
    This takes out the hard-coded table references and just uses classes
    instead

--- nova/modules/core/views/_base/main/js/personnel_index_js.php
+++ nova/modules/core/views/_base/main/js/personnel_index_js.php
@@ -3,93 +3,91 @@
 /* strip out the comma from the string */
 $manifest_default_values = str_replace(',', '', $manifest_defaults);
 
-if(isset($display))
+if (isset($display))
 {
-	if($display == "crew")
+	if ($display == "crew")
 	{
-		$manifest_default_values = "$('tr.open').hide();";
-		$manifest_default_values.= "$('tr.npc').hide();";
+		$manifest_default_values = "$('.open').hide();";
+		$manifest_default_values.= "$('.npc').hide();";
 	}
-	if($display == "npcs")
+	if ($display == "npcs")
 	{
-		$manifest_default_values = "$('tr.active').hide();";
-		$manifest_default_values.= "$('tr.npc').show();";
+		$manifest_default_values = "$('.active').hide();";
+		$manifest_default_values.= "$('.npc').show();";
 	}
-	if($display == "past")
+	if ($display == "past")
 	{
-		$manifest_default_values = "$('tr.active').hide();";
-		$manifest_default_values.= "$('tr.inactive').show();";
+		$manifest_default_values = "$('.active').hide();";
+		$manifest_default_values.= "$('.inactive').show();";
 	}
-	if($display == "open")
+	if ($display == "open")
 	{
-		$manifest_default_values = "$('tr.active').hide();";
-		$manifest_default_values.= "$('tr.open').show();";
+		$manifest_default_values = "$('.active').hide();";
+		$manifest_default_values.= "$('.open').show();";
 	}
 }
 
-?>
-
-<script type="text/javascript">
+?><script type="text/javascript">
 	$(document).ready(function() {
 		<?php echo $manifest_default_values; ?>
 		
 		$('#all').click(function() {
-			$('tr.inactive').hide();
-			$('tr.open').hide();
+			$('.inactive').hide();
+			$('.open').hide();
 			$('#top-open').hide();
 			
-			$('tr.active').show();
-			$('tr.npc').show();
+			$('.active').show();
+			$('.npc').show();
 			return false;
 		});
 		
 		$('#active').click(function() {
-			$('tr.inactive').hide();
-			$('tr.npc').hide();
-			$('tr.open').hide();
+			$('.inactive').hide();
+			$('.npc').hide();
+			$('.open').hide();
 			$('#top-open').hide();
 			
-			$('tr.active').show();
+			$('.active').show();
 			return false;
 		});
 		
 		$('#npc').click(function() {
-			$('tr.inactive').hide();
-			$('tr.active').hide();
-			$('tr.open').hide();
+			$('.inactive').hide();
+			$('.active').hide();
+			$('.open').hide();
 			$('#top-open').hide();
 			
-			$('tr.npc').show();
+			$('.npc').show();
 			return false;
 		});
 		
 		$('#inactive').click(function() {
-			$('tr.active').hide();
-			$('tr.npc').hide();
-			$('tr.open').hide();
+			$('.active').hide();
+			$('.npc').hide();
+			$('.open').hide();
 			$('#top-open').hide();
 			
-			$('tr.inactive').show();
+			$('.inactive').show();
 			return false;
 		});
 		
 		$('#open').click(function() {
-			$('tr.active').hide();
-			$('tr.npc').hide();
-			$('tr.inactive').hide();
+			$('.active').hide();
+			$('.npc').hide();
+			$('.inactive').hide();
 			
-			$('tr.open').show();
+			$('.open').show();
 			$('#top-open').show();
 			return false;
 		});
 		
 		$('#toggle_open').click(function() {
-			$('tr.open').toggle();
+			$('.open').toggle();
 			return false;
 		});
 		
 		$('#toggle_npc').click(function() {
-			$('tr.npc').toggle();
+			$('.npc').toggle();
 			return false;
 		});
 		
@@ -99,7 +97,7 @@ if(isset($display))
 			placement: 'right'
 		});
 		
-		$('#loader').hide(); /* hide the loader */
-		$('#manifest').removeClass('hidden'); /* show the manifest */
+		$('#loader').hide();
+		$('#manifest').removeClass('hidden');
 	});
 </script>
