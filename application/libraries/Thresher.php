f72ab730ae98afb1cfc51aac3cdaf6dd366896c1
David VanScott <david.vanscott@gmail.com>
2018-05-12 09:48:33 -0400

    Fix constructor

--- application/libraries/Thresher.php
+++ application/libraries/Thresher.php
@@ -1,4 +1,4 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /**
  * Thresher library
  *
@@ -10,10 +10,10 @@
  */
 
 class Thresher {
-	
+
 	var $parsetype = 'html';
-	
-	function __constructor($params = array())
+
+	function __construct($params = array())
 	{
 		if (is_array($params))
 		{
@@ -22,62 +22,62 @@ class Thresher {
 				$this->$key = $value;
 			}
 		}
-		
+
 		/* log the debug message */
 		log_message('debug', 'Thresher Library Initialized');
 	}
-	
+
 	function parse($text = '')
 	{
 		$retval = $text;
-		
+
 		switch ($this->parsetype)
 		{
 			case 'bbcode':
 				$retval = $this->_bbcode($text);
 			break;
-				
+
 			case 'html':
 				$retval = $this->_html($text);
 			break;
-				
+
 			case 'markdown':
 				$retval = $this->_markdown($text);
 			break;
-				
+
 			case 'textile':
 				$retval = $this->_textile($text);
 			break;
 		}
-		
+
 		return $retval;
 	}
-	
+
 	function _bbcode($text = '')
 	{
 		include_once APPPATH .'libraries/Thresher_BBCode.php';
-		
+
 		return bbcode($text);
 	}
-	
+
 	function _html($text = '')
 	{
 		return $text;
 	}
-	
+
 	function _markdown($text = '')
 	{
 		include_once APPPATH .'libraries/Thresher_Markdown.php';
-		
+
 		return Markdown($text);
 	}
-	
+
 	function _textile($text = '')
 	{
 		include_once APPPATH .'libraries/Thresher_Textile.php';
-		
+
 		$textile = new Textile();
-		
+
 		return $textile->TextileThis($text);
 	}
 }
