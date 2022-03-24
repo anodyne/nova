ec8924714daf00d4e5c715954a23bc47a9f4b35b
David VanScott <david.vanscott@gmail.com>
2022-03-24 09:57:42 -0400

    Add new language

---
+++ application/language/english/app_lang.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+ * English language file
+ *
+ * Punctuation contants are defined in application/config/constants.php
+ *
+ * @package		Nova
+ * @category	Language
+ * @author		Anodyne Productions
+ * @copyright	2010-11 Anodyne Productions
+ * @version		2.0
+ */
+
+// figure out what language the file is
+$language = basename(dirname(__FILE__));
+
+// include the base language file
+include_once MODPATH.'core/language/'.$language.'/base_lang.php';
+
+/*
+ * Your language array keys go here in the following format:
+ *
+ * $lang['key'] = 'My Key';
+ *
+ * If you want to override an existing key, you can do so
+ * by redeclaring it like this:
+ *
+ * $lang['global_position'] = 'job';
+ *
+ * An example item is below for you to copy and paste.
+ */
+
+//$lang['global_position'] = 'job';
