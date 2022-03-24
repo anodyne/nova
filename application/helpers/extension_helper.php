e6ac44fb42ce02127b9f261a90d37907c5c10f77
David VanScott <david.vanscott@gmail.com>
2022-03-24 09:56:56 -0400

    Add new helpers

---
+++ application/helpers/extension_helper.php
@@ -0,0 +1,7 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+require_once MODPATH.'core/helpers/Nova_extension_helper.php';
+
+class Extension_container extends Nova_extension_container {}
+
+class Extension_initialization_context extends Nova_extension_initialization_context {}
