46af128f88bb5402d2a32d6535bb57a2a20b769f
David VanScott <david.vanscott@gmail.com>
2022-03-24 11:39:08 -0400

    Update the format of the example mysql config

--- nova/modules/assets/database/db.mysqli.php
+++ nova/modules/assets/database/db.mysqli.php
@@ -4,24 +4,22 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = true;
 
-$db['default'] = array(
-    'dsn'	=> '',
-    'hostname' => 'localhost',
-    'username' => 'novauser',
-    'password' => 'novapass',
-    'database' => 'novadb',
-    'dbdriver' => 'mysqli',
-    'dbprefix' => 'nova_',
-    'pconnect' => false,
-    'db_debug' => (ENVIRONMENT !== 'production'),
-    'cache_on' => false,
-    'cachedir' => '',
-    'char_set' => 'utf8',
-    'dbcollat' => 'utf8_general_ci',
-    'swap_pre' => '',
-    'encrypt' => false,
-    'compress' => false,
-    'stricton' => false,
-    'failover' => array(),
-    'save_queries' => true
-);
+$db['default']['dsn'] = '';
+$db['default']['hostname'] = 'localhost';
+$db['default']['username'] = 'novauser';
+$db['default']['password'] = 'novapass';
+$db['default']['database'] = 'novadb';
+$db['default']['dbdriver'] = 'mysqli';
+$db['default']['dbprefix'] = 'nova_';
+$db['default']['pconnect'] = false;
+$db['default']['db_debug'] = (ENVIRONMENT !== 'production');
+$db['default']['cache_on'] = false;
+$db['default']['cachedir'] = '';
+$db['default']['char_set'] = 'utf8';
+$db['default']['dbcollat'] = 'utf8_general_ci';
+$db['default']['swap_pre'] = '';
+$db['default']['encrypt'] = false;
+$db['default']['compress'] = false;
+$db['default']['stricton'] = false;
+$db['default']['failover'] = [];
+$db['default']['save_queries'] = true;
