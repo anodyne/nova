086caf947597b96823f730fbc9ada5688be003cb
David VanScott <david.vanscott@gmail.com>
2022-10-14 13:33:25 -0400

    Style updates for search

--- nova/modules/core/views/_base/main/pages/search_index.php
+++ nova/modules/core/views/_base/main/pages/search_index.php
@@ -1,23 +1,25 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+<?php if (! defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}?>
 
 <?php echo text_output($header, 'h1', 'page-head');?>
 
 <?php echo form_open('search/results');?>
 	<p>
 		<kbd><?php echo $label['type'];?></kbd>
-		<?php echo form_dropdown('type', $type);?>
+		<?php echo form_dropdown('type', $type, [], 'class="large"');?>
 	</p>
-	
+
 	<p>
 		<kbd><?php echo $label['search_in'];?></kbd>
-		<?php echo form_dropdown('component', $component);?>
+		<?php echo form_dropdown('component', $component, [], 'class="large"');?>
 	</p>
-	
+
 	<p>
 		<kbd><?php echo $label['search_for'];?></kbd>
 		<?php echo form_input($inputs['search']);?>
 	</p><br />
-	
+
 	<p>
 		<?php echo form_button($inputs['submit']);?>
 	</p>
