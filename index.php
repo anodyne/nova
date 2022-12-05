71f8e9175ac2224df0b6ba30eef92fb6207d34e7
David VanScott <david.vanscott@gmail.com>
2022-12-04 21:55:03 -0500

    Make sure production is the default environment

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ define('INSTALL_ROOT', str_replace('\\', '/', realpath(dirname(__FILE__))).'/nov
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-    define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+define('ENVIRONMENT', 'production');
 
 /*
  *---------------------------------------------------------------
@@ -69,7 +69,7 @@ switch (ENVIRONMENT) {
     case 'development':
         error_reporting(-1);
         ini_set('display_errors', 1);
-    break;
+        break;
 
     case 'testing':
     case 'production':
@@ -79,7 +79,7 @@ switch (ENVIRONMENT) {
         } else {
             error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
         }
-    break;
+        break;
 
     default:
         header('HTTP/1.1 503 Service Unavailable.', true, 503);
@@ -95,7 +95,7 @@ switch (ENVIRONMENT) {
  * This variable must contain the name of your "system" directory.
  * Set the path if it is not in the same directory as this file.
  */
-    $system_path = INSTALL_ROOT.'ci';
+$system_path = INSTALL_ROOT.'ci';
 
 /*
  *---------------------------------------------------------------
@@ -112,7 +112,7 @@ switch (ENVIRONMENT) {
  *
  * NO TRAILING SLASH!
  */
-    $application_folder = 'application';
+$application_folder = 'application';
 
 /**
  * Modules folder name
@@ -139,7 +139,7 @@ $modules_folder = INSTALL_ROOT.$mods_folder;
  *
  * NO TRAILING SLASH!
  */
-    $view_folder = '';
+$view_folder = '';
 
 
 /*
@@ -161,15 +161,15 @@ $modules_folder = INSTALL_ROOT.$mods_folder;
  *
  * Un-comment the $routing array below to use this feature
  */
-    // The directory name, relative to the "controllers" directory.  Leave blank
-    // if your controller is not in a sub-directory within the "controllers" one
-    // $routing['directory'] = '';
+// The directory name, relative to the "controllers" directory.  Leave blank
+// if your controller is not in a sub-directory within the "controllers" one
+// $routing['directory'] = '';
 
-    // The controller class file name.  Example:  mycontroller
-    // $routing['controller'] = '';
+// The controller class file name.  Example:  mycontroller
+// $routing['controller'] = '';
 
-    // The controller function you wish to be called.
-    // $routing['function']	= '';
+// The controller function you wish to be called.
+// $routing['function']	= '';
 
 
 /*
@@ -186,7 +186,7 @@ $modules_folder = INSTALL_ROOT.$mods_folder;
  *
  * Un-comment the $assign_to_config array below to use this feature
  */
-    // $assign_to_config['name_of_config_item'] = 'value of config item';
+// $assign_to_config['name_of_config_item'] = 'value of config item';
 
 
 
@@ -200,110 +200,110 @@ $modules_folder = INSTALL_ROOT.$mods_folder;
  * ---------------------------------------------------------------
  */
 
-    // Set the current directory correctly for CLI requests
-    if (defined('STDIN')) {
-        chdir(dirname(__FILE__));
-    }
+// Set the current directory correctly for CLI requests
+if (defined('STDIN')) {
+    chdir(dirname(__FILE__));
+}
 
-    if (($_temp = realpath($system_path)) !== false) {
-        $system_path = $_temp.DIRECTORY_SEPARATOR;
-    } else {
-        // Ensure there's a trailing slash
-        $system_path = strtr(
-            rtrim($system_path, '/\\'),
-            '/\\',
-            DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-        ).DIRECTORY_SEPARATOR;
-    }
+if (($_temp = realpath($system_path)) !== false) {
+    $system_path = $_temp.DIRECTORY_SEPARATOR;
+} else {
+    // Ensure there's a trailing slash
+    $system_path = strtr(
+        rtrim($system_path, '/\\'),
+        '/\\',
+        DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+    ).DIRECTORY_SEPARATOR;
+}
 
-    // Is the system path correct?
-    if (! is_dir($system_path)) {
-        header('HTTP/1.1 503 Service Unavailable.', true, 503);
-        echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: '.pathinfo(__FILE__, PATHINFO_BASENAME);
-        exit(3); // EXIT_CONFIG
-    }
+// Is the system path correct?
+if (! is_dir($system_path)) {
+    header('HTTP/1.1 503 Service Unavailable.', true, 503);
+    echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: '.pathinfo(__FILE__, PATHINFO_BASENAME);
+    exit(3); // EXIT_CONFIG
+}
 
 /*
  * -------------------------------------------------------------------
  *  Now that we know the path, set the main path constants
  * -------------------------------------------------------------------
  */
-    // The name of THIS file
-    define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
+// The name of THIS file
+define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
 
-    // Path to the system directory
-    define('BASEPATH', $system_path);
+// Path to the system directory
+define('BASEPATH', $system_path);
 
-    // Path to the front controller (this file) directory
-    define('FCPATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
+// Path to the front controller (this file) directory
+define('FCPATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
 
-    // Name of the "system" directory
-    define('SYSDIR', basename(BASEPATH));
+// Name of the "system" directory
+define('SYSDIR', basename(BASEPATH));
 
-    define('APPFOLDER', $application_folder);
-    define('MODFOLDER', 'nova/'.$mods_folder);
+define('APPFOLDER', $application_folder);
+define('MODFOLDER', 'nova/'.$mods_folder);
 
-    // The path to the "application" directory
-    if (is_dir($application_folder)) {
-        if (($_temp = realpath($application_folder)) !== false) {
-            $application_folder = $_temp;
-        } else {
-            $application_folder = strtr(
-                rtrim($application_folder, '/\\'),
-                '/\\',
-                DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-            );
-        }
-    } elseif (is_dir(BASEPATH.$application_folder.DIRECTORY_SEPARATOR)) {
-        $application_folder = BASEPATH.strtr(
-            trim($application_folder, '/\\'),
+// The path to the "application" directory
+if (is_dir($application_folder)) {
+    if (($_temp = realpath($application_folder)) !== false) {
+        $application_folder = $_temp;
+    } else {
+        $application_folder = strtr(
+            rtrim($application_folder, '/\\'),
             '/\\',
             DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
         );
-    } else {
-        header('HTTP/1.1 503 Service Unavailable.', true, 503);
-        echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
-        exit(3); // EXIT_CONFIG
     }
+} elseif (is_dir(BASEPATH.$application_folder.DIRECTORY_SEPARATOR)) {
+    $application_folder = BASEPATH.strtr(
+        trim($application_folder, '/\\'),
+        '/\\',
+        DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+    );
+} else {
+    header('HTTP/1.1 503 Service Unavailable.', true, 503);
+    echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.self;
+    exit(3); // EXIT_CONFIG
+}
 
-    define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
+define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
 
-    // The path to the "views" directory
-    if (! isset($view_folder[0]) && is_dir(APPPATH.'views'.DIRECTORY_SEPARATOR)) {
-        $view_folder = APPPATH.'views';
-    } elseif (is_dir($view_folder)) {
-        if (($_temp = realpath($view_folder)) !== false) {
-            $view_folder = $_temp;
-        } else {
-            $view_folder = strtr(
-                rtrim($view_folder, '/\\'),
-                '/\\',
-                DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-            );
-        }
-    } elseif (is_dir(APPPATH.$view_folder.DIRECTORY_SEPARATOR)) {
-        $view_folder = APPPATH.strtr(
-            trim($view_folder, '/\\'),
+// The path to the "views" directory
+if (! isset($view_folder[0]) && is_dir(APPPATH.'views'.DIRECTORY_SEPARATOR)) {
+    $view_folder = APPPATH.'views';
+} elseif (is_dir($view_folder)) {
+    if (($_temp = realpath($view_folder)) !== false) {
+        $view_folder = $_temp;
+    } else {
+        $view_folder = strtr(
+            rtrim($view_folder, '/\\'),
             '/\\',
             DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
         );
-    } else {
-        header('HTTP/1.1 503 Service Unavailable.', true, 503);
-        echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
-        exit(3); // EXIT_CONFIG
+    }
+} elseif (is_dir(APPPATH.$view_folder.DIRECTORY_SEPARATOR)) {
+    $view_folder = APPPATH.strtr(
+        trim($view_folder, '/\\'),
+        '/\\',
+        DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
+    );
+} else {
+    header('HTTP/1.1 503 Service Unavailable.', true, 503);
+    echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.self;
+    exit(3); // EXIT_CONFIG
+}
+
+define('VIEWPATH', $view_folder.DIRECTORY_SEPARATOR);
+
+if (is_dir($modules_folder)) {
+    define('MODPATH', $modules_folder.'/');
+} else {
+    if ($modules_folder == '') {
+        $modules_folder = 'modules';
     }
 
-    define('VIEWPATH', $view_folder.DIRECTORY_SEPARATOR);
-
-    if (is_dir($modules_folder)) {
-        define('MODPATH', $modules_folder.'/');
-    } else {
-        if ($modules_folder == '') {
-            $modules_folder = 'modules';
-        }
-
-        define('MODPATH', INSTALL_ROOT.$modules_folder.'/');
-    }
+    define('MODPATH', INSTALL_ROOT.$modules_folder.'/');
+}
 
 
 if (phpversion() < '7.0') {
