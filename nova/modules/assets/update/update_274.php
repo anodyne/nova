37050628e1e5d65d9c77c4ceb5f70c00a1315da8
David VanScott <david.vanscott@gmail.com>
2023-03-09 18:24:39 -0500

    Updates to fix nullable fields

--- nova/modules/assets/update/update_274.php
+++ nova/modules/assets/update/update_274.php
@@ -131,6 +131,276 @@ if ($add_column !== null) {
 |---------------------------------------------------------------
 */
 
+$nullable = ['null' => true];
+$userField = [
+    'type' => 'INT',
+    'constraint' => 8,
+];
+$characterField = [
+    'type' => 'INT',
+    'constraint' => 8,
+];
+$dateField = [
+    'type' => 'BIGINT',
+    'constraint' => 20,
+];
+$textField = [
+    'type' => 'TEXT',
+];
+
+
+$modify_column = [
+    'access_pages' => [
+        'page_desc' => array_merge($nullable, $textField),
+    ],
+    'access_roles' => [
+        'role_access' => array_merge($nullable, $textField),
+        'role_desc' => array_merge($nullable, $textField),
+    ],
+    'applications' => [
+        'app_character_name' => array_merge($nullable, $textField),
+        'app_message' => array_merge($nullable, $textField),
+    ],
+    'awards' => [
+        'award_desc' => array_merge($nullable, $textField),
+    ],
+    'awards_queue' => [
+        'queue_reason' => array_merge($nullable, $textField),
+    ],
+    'awards_received' => [
+        'awardrec_reason' => array_merge($nullable, $textField),
+    ],
+    'bans' => [
+        'ban_reason' => array_merge($nullable, $textField),
+    ],
+    'catalogue_ranks' => [
+        'rankcat_credits' => array_merge($nullable, $textField),
+        'rankcat_url' => array_merge($nullable, $textField),
+    ],
+    'catalogue_skins' => [
+        'skin_credits' => array_merge($nullable, $textField),
+    ],
+    'characters' => [
+        'user' => array_merge($nullable, $userField),
+        'date_activate' => array_merge($nullable, $dateField),
+        'date_deactivate' => array_merge($nullable, $dateField),
+        'images' => array_merge($nullable, $textField),
+        'rank' => array_merge($nullable, ['type' => 'INT', 'constraint' => 10]),
+        'position_2' => array_merge($nullable, ['type' => 'INT', 'constraint' => 10]),
+        'last_post' => array_merge($nullable, $dateField),
+    ],
+    'characters_data' => [
+        'data_user' => array_merge($nullable, $userField),
+        'data_value' => array_merge($nullable, $textField),
+    ],
+    'characters_fields' => [
+        'field_type' => array_merge($nullable, [
+            'type' => 'ENUM',
+            'constraint' => ['text','select','textarea'],
+            'default' => 'text',
+        ]),
+        'field_class' => array_merge($nullable, $textField),
+        'field_label_page' => array_merge($nullable, $textField),
+        'field_value' => array_merge($nullable, $textField),
+        'field_help' => array_merge($nullable, $textField),
+    ],
+    'characters_promotions' => [
+        'prom_user' => array_merge($nullable, $userField),
+    ],
+    'characters_values' => [
+        'value_content' => array_merge($nullable, $textField),
+    ],
+    'departments' => [
+        'dept_desc' => array_merge($nullable, $textField),
+    ],
+    'docking' => [
+        'docking_sim_url' => array_merge($nullable, $textField),
+    ],
+    'docking_data' => [
+        'data_value' => array_merge($nullable, $textField),
+    ],
+    'docking_fields' => [
+        'field_type' => array_merge($nullable, [
+            'type' => 'ENUM',
+            'constraint' => ['text','select','textarea'],
+            'default' => 'text',
+        ]),
+        'field_class' => array_merge($nullable, $textField),
+        'field_label_page' => array_merge($nullable, $textField),
+        'field_value' => array_merge($nullable, $textField),
+        'field_help' => array_merge($nullable, $textField),
+    ],
+    'docking_values' => [
+        'value_content' => array_merge($nullable, $textField),
+    ],
+    'manifests' => [
+        'manifest_desc' => array_merge($nullable, $textField),
+        'manifest_header_content' => array_merge($nullable, $textField),
+        'manifest_view' => array_merge($nullable, $textField),
+        'manifest_metadata' => array_merge($nullable, $textField),
+    ],
+    'menu_items' => [
+        'menu_link' => array_merge($nullable, $textField),
+    ],
+    'messages' => [
+        'message_content' => array_merge($nullable, $textField),
+    ],
+    'mission_groups' => [
+        'misgroup_desc' => array_merge($nullable, $textField),
+    ],
+    'missions' => [
+        'mission_images' => array_merge($nullable, $textField),
+        'mission_group' => array_merge($nullable, ['type' => 'INT', 'constraint' => 5]),
+        'mission_start' => array_merge($nullable, $dateField),
+        'mission_end' => array_merge($nullable, $dateField),
+        'mission_desc' => array_merge($nullable, $textField),
+        'mission_summary' => array_merge($nullable, $textField),
+        'mission_notes' => array_merge($nullable, $textField),
+        'mission_notes_updated' => array_merge($nullable, $dateField),
+    ],
+    'news' => [
+        'news_content' => array_merge($nullable, $textField),
+        'news_tags' => array_merge($nullable, $textField),
+    ],
+    'news_comments' => [
+        'ncomment_content' => array_merge($nullable, $textField),
+    ],
+    'personallogs' => [
+        'log_content' => array_merge($nullable, $textField),
+        'log_tags' => array_merge($nullable, $textField),
+    ],
+    'personallogs_comments' => [
+        'lcomment_content' => array_merge($nullable, $textField),
+    ],
+    'positions' => [
+        'pos_desc' => array_merge($nullable, $textField),
+    ],
+    'posts' => [
+        'post_authors' => array_merge($nullable, $textField),
+        'post_authors_users' => array_merge($nullable, $textField),
+        'post_saved' => array_merge($nullable, $userField),
+        'post_content' => array_merge($nullable, $textField),
+        'post_tags' => array_merge($nullable, $textField),
+        'post_participants' => array_merge($nullable, $textField),
+        'post_lock_user' => array_merge($nullable, $userField),
+        'post_lock_date' => array_merge($nullable, $dateField),
+    ],
+    'posts_comments' => [
+        'pcomment_content' => array_merge($nullable, $textField),
+    ],
+    'privmsgs' => [
+        'privmsgs_content' => array_merge($nullable, $textField),
+    ],
+    'security_questions' => [
+        'question_value' => array_merge($nullable, $textField),
+    ],
+    'settings' => [
+        'setting_value' => array_merge($nullable, $textField),
+    ],
+    'specs' => [
+        'specs_images' => array_merge($nullable, $textField),
+        'specs_summary' => array_merge($nullable, $textField),
+    ],
+    'specs_data' => [
+        'data_value' => array_merge($nullable, $textField),
+        'data_updated' => array_merge($nullable, $dateField),
+    ],
+    'specs_fields' => [
+        'field_type' => array_merge($nullable, [
+            'type' => 'ENUM',
+            'constraint' => ['text','select','textarea'],
+            'default' => 'text',
+        ]),
+        'field_class' => array_merge($nullable, $textField),
+        'field_label_page' => array_merge($nullable, $textField),
+        'field_value' => array_merge($nullable, $textField),
+        'field_help' => array_merge($nullable, $textField),
+    ],
+    'specs_values' => [
+        'value_content' => array_merge($nullable, $textField),
+    ],
+    'system_info' => [
+        'sys_last_update' => array_merge($nullable, $dateField),
+    ],
+    'tour' => [
+        'tour_images' => array_merge($nullable, $textField),
+        'tour_summary' => array_merge($nullable, $textField),
+    ],
+    'tour_data' => [
+        'data_value' => array_merge($nullable, $textField),
+        'data_updated' => array_merge($nullable, $dateField),
+    ],
+    'tour_fields' => [
+        'field_type' => array_merge($nullable, [
+            'type' => 'ENUM',
+            'constraint' => ['text','select','textarea'],
+            'default' => 'text',
+        ]),
+        'field_class' => array_merge($nullable, $textField),
+        'field_label_page' => array_merge($nullable, $textField),
+        'field_value' => array_merge($nullable, $textField),
+        'field_help' => array_merge($nullable, $textField),
+    ],
+    'tour_values' => [
+        'value_content' => array_merge($nullable, $textField),
+    ],
+    'tour_decks' => [
+        'deck_content' => array_merge($nullable, $textField),
+    ],
+    'uploads' => [
+        'upload_filename' => array_merge($nullable, $textField),
+    ],
+    'user_loa' => [
+        'loa_start_date' => array_merge($nullable, $dateField),
+        'loa_end_date' => array_merge($nullable, $dateField),
+        'loa_duration' => array_merge($nullable, $textField),
+        'loa_reason' => array_merge($nullable, $textField),
+    ],
+    'user_prefs' => [
+        'pref_default' => array_merge($nullable, $textField),
+    ],
+    'user_prefs_values' => [
+        'prefvalue_value' => array_merge($nullable, $textField),
+    ],
+    'users' => [
+        'instant_message' => array_merge($nullable, $textField),
+        'main_char' => array_merge($nullable, $characterField),
+        'access_role' => array_merge($nullable, ['type' => 'INT', 'constraint' => 5]),
+        'join_date' => array_merge($nullable, $dateField),
+        'leave_date' => array_merge($nullable, $dateField),
+        'last_post' => array_merge($nullable, $dateField),
+        'last_login' => array_merge($nullable, $dateField),
+        'location' => array_merge($nullable, $textField),
+        'interests' => array_merge($nullable, $textField),
+        'bio' => array_merge($nullable, $textField),
+        'my_links' => array_merge($nullable, $textField),
+        'last_update' => array_merge($nullable, $dateField),
+    ],
+    'wiki_categories' => [
+        'wikicat_desc' => array_merge($nullable, $textField),
+    ],
+    'wiki_comments' => [
+        'wcomment_content' => array_merge($nullable, $textField),
+    ],
+    'wiki_drafts' => [
+        'draft_id_old' => array_merge($nullable, ['type' => 'INT', 'constraint' => 10]),
+        'draft_summary' => array_merge($nullable, $textField),
+        'draft_content' => array_merge($nullable, $textField),
+        'draft_categories' => array_merge($nullable, $textField),
+        'draft_changed_comments' => array_merge($nullable, $textField),
+    ],
+    'wiki_pages' => [
+        'page_updated_at' => array_merge($nullable, $dateField),
+        'page_updated_by_user' => array_merge($nullable, $userField),
+        'page_updated_by_character' => array_merge($nullable, $characterField),
+    ],
+    'wiki_restrictions' => [
+        'restr_updated_at' => array_merge($nullable, $dateField),
+        'restr_updated_by' => array_merge($nullable, $userField),
+        'restrictions' => array_merge($nullable, $textField),
+    ],
+];
+
 if ($modify_column !== null) {
     foreach ($modify_column as $key => $value) {
         $this->dbforge->modify_column($key, $value);
