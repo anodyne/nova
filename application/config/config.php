8be35415ff4b35c3d1d6d889ad41c9396bd58dae
David VanScott <david.vanscott@gmail.com>
2024-03-15 19:02:28 -0400

    Messaging for the config file for who's online

--- application/config/config.php
+++ application/config/config.php
@@ -3,3 +3,10 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 
 require_once MODPATH.'core/config/nova_config.php';
+
+/**
+ * If you want to use the database driver for sessions to enable the Who's Online
+ * feature, simply remove the double slashes from the line below.
+ */
+
+//$config['sess_driver'] = 'database';
